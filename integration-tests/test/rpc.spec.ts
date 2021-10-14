392d9ff6843d0f6cc7d8854cd58af26a8462a256
souradeep-das <dsouradeep2@gmail.com>
2021-10-14 18:13:58 +0530

    integ

--- integration-tests/test/rpc.spec.ts
+++ integration-tests/test/rpc.spec.ts
@@ -240,7 +240,7 @@ describe('Basic RPC tests', () => {
     it('includes L1 gas price and L1 gas used', async () => {
       const tx = await env.l2Wallet.populateTransaction({
         to: env.l2Wallet.address,
-        gasPrice: 1,
+        gasPrice: 20000000,
       })
 
       const raw = serialize({
