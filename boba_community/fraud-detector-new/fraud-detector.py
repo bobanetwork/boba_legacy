1ad5ac7bbec01204e15e3d49d2e7df9c71beb9ce
Michael Montour <mmontour@enya.ai>
2021-11-01 16:04:10 -0700

    Fraud detector - initial checkin
    This utility scans state roots from the SCC contract and compares
    them against both the main L2geth and an independent verifier node.
    It comes with a custom dockerfile to build a standalone instance
    of a DTL and L2geth configured in "verifier" mode (sync from L1).
    
     Changes to be committed:
           boba_community/fraud-detector-new/contracts/StateCommitmentChain.json
           boba_community/fraud-detector-new/deployments/README.md
           boba_community/fraud-detector-new/deployments/local/env
           boba_community/fraud-detector-new/deployments/local/state-dump.latest.json
           boba_community/fraud-detector-new/deployments/mainnet-v2/env
           boba_community/fraud-detector-new/deployments/mainnet-v2/state-dump.latest.json
           boba_community/fraud-detector-new/docker-compose-fraud-detector.yml
           boba_community/fraud-detector-new/docker/Dockerfile.fraud-detector
           boba_community/fraud-detector-new/docker/Dockerfile.verifier_dtl
           boba_community/fraud-detector-new/fraud-detector.py

---
+++ boba_community/fraud-detector-new/fraud-detector.py
@@ -0,0 +1,163 @@
+#!python
+#
+# Copyright 2021 mmontour@enya.ai. Internal use only; all rights reserved
+#
+# Basic fraud checker. Reads state roots from SCC and checks them against
+# actual L2 values, reporting mismatches. Note - code was copied from
+# stress_tester.py and contains a lot of irrelevant cruft to be cleaned up
+# later. 
+
+import os,sys
+from web3 import Web3
+import signal
+import time
+import requests,json
+import logging
+from web3.middleware import geth_poa_middleware
+from web3.logs import STRICT, IGNORE, DISCARD, WARN
+
+logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', datefmt='%Y%m%dT%H%M%S')
+logger = logging.getLogger('fraud-detector')
+logger.setLevel(logging.DEBUG)
+#logger.addHandler(logging.StreamHandler())
+
+logger.debug (os.environ['L1_NODE_WEB3_URL'])
+logger.debug (os.environ['L2_NODE_WEB3_URL'])
+logger.debug (os.environ['VERIFIER_WEB3_URL'])
+logger.debug (os.environ['ADDRESS_MANAGER_ADDRESS'])
+logger.debug (os.environ['L1_MAINNET_DEPLOYMENT_BLOCK'])
+
+l1_base = int(os.environ['L1_MAINNET_DEPLOYMENT_BLOCK'])
+element_start = 1
+
+try:
+  with open("./checkpoint.dat", "r") as f:
+    start_at = json.loads(f.read())
+    element_start = start_at[0]
+    l1_base = start_at[1]
+    logger.info("Starting at checkpoint index " + str(element_start) + ", l1_block " + str(l1_base))
+except:
+  pass
+
+batch_size =1000
+
+rpc = [None]*4
+rpc[1] = Web3(Web3.HTTPProvider(os.environ['L1_NODE_WEB3_URL']))
+assert (rpc[1].isConnected())
+logger.debug ("Connected to L1_NODE_WEB3_URL")
+
+rpc[2] = Web3(Web3.HTTPProvider(os.environ['L2_NODE_WEB3_URL']))
+assert (rpc[2].isConnected())
+rpc[2].middleware_onion.inject(geth_poa_middleware, layer=0)
+logger.debug("Connected to L2_NODE_WEB3_URL")
+
+while True:
+  try:
+    rpc[3] = Web3(Web3.HTTPProvider(os.environ['VERIFIER_WEB3_URL']))
+    assert (rpc[3].isConnected())
+    break
+  except:
+    logger.info ("Waiting for verifier...")
+    time.sleep(10)
+
+rpc[3].middleware_onion.inject(geth_poa_middleware, layer=0)
+logger.debug("Connected to VERIFIER_WEB3_URL")
+
+def loadContract(rpc, addr, abiPath):
+  with open(abiPath) as f:
+    abi = json.loads(f.read())['abi']
+  c = rpc.eth.contract(address=addr, abi=abi)
+  return c
+
+address_manager = loadContract(rpc[1],os.environ['ADDRESS_MANAGER_ADDRESS'],'./contracts/Lib_AddressManager.json')
+scc_addr = address_manager.functions.getAddress("StateCommitmentChain").call()
+scc_contract = loadContract(rpc[1],scc_addr,'./contracts/StateCommitmentChain.json')
+
+rCount = element_start - 1
+checkpoint = [ element_start, l1_base ]
+last_saved = l1_base
+
+def doEvent(event):
+  global rCount
+
+  t = rpc[1].eth.get_transaction(event.transactionHash)
+
+  (junk, ib) = scc_contract.decode_function_input(t.input)
+  for sr in ib['_batch']:
+    rCount += 1
+    l2b = rpc[2].eth.getBlock(rCount)
+    l2SR = l2b['stateRoot']
+
+    vfb = rpc[3].eth.getBlock(rCount)
+    vfSR = vfb['stateRoot']
+
+    match = ""
+    if sr != l2SR:
+      match = "**** SCC/L2 MISMATCH ****"
+    elif l2SR != vfSR:
+      match = "**** L2/VERIFIER MISMATCH ****"
+    log_str = "{} {} {} {} {} {}".format(rCount, event.blockNumber, Web3.toHex(sr), Web3.toHex(l2SR), Web3.toHex(vfSR), match)
+    if match != "":
+      logger.warn(log_str)
+    else:
+      logger.info(log_str)
+
+    if event.blockNumber > checkpoint[1]:
+      checkpoint[0] = rCount
+      checkpoint[1] = event.blockNumber
+
+def do_checkpoint():
+  global last_saved
+  if last_saved != checkpoint[1]:
+    with open("./checkpoint.dat", "w") as f:
+      f.write(json.dumps(checkpoint))
+    last_saved = checkpoint[1]
+
+def fpLoop():  
+  l1_tip = rpc[1].eth.block_number
+  assert(l1_tip > l1_base)
+  startBlock = l1_base
+  logger.debug("SCC contract at {}, l1_base {}, l1_tip []".format(scc_addr, l1_base, l1_tip))
+
+  logger.info("#SCC-IDX L1-Block SCC-STATEROOT L2-STATEROOT VERIFIER-STATEROOT MISMATCH")
+
+  topic_sig = Web3.toHex(Web3.keccak(text="StateBatchAppended(uint256,bytes32,uint256,uint256,bytes)"))
+
+  while startBlock < l1_tip:
+    toBlock = min(startBlock+batch_size, l1_tip) - 1
+    #print("Scanning from",startBlock,"to",toBlock)
+
+    FF = rpc[1].eth.filter({
+      "fromBlock":startBlock,
+      "toBlock":toBlock,
+      "address":scc_addr,
+      "topics":[topic_sig]
+    })
+
+    for event in FF.get_all_entries():
+      doEvent(event)
+
+    rpc[1].eth.uninstall_filter(FF.filter_id)
+
+    startBlock = toBlock + 1
+    do_checkpoint()
+
+  FF = rpc[1].eth.filter({
+      "fromBlock":startBlock,
+      "toBlock":'latest',
+      "address":scc_addr,
+      "topics":[topic_sig]
+  })
+
+  while True:
+    # FIXME - if the L1 node restarts, this can fail with "filter not found". May want to
+    # put some retry/recovery logic inside this utility rather than relying on an external
+    # service to restart it. 
+    for event in FF.get_new_entries():
+      doEvent(event)
+    do_checkpoint()
+    time.sleep(30)
+
+  logger.info("Exiting")
+
+fpLoop()
