493e614f18ba3452aee1c2b019ec3955aa779f3f
Petar Denev <pdenev@gmail.com>
2021-12-10 21:02:11 +0100

    fixing ../go/proxyd/entrypoint.sh

--- go/proxyd/entrypoint.sh
+++ go/proxyd/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Replacing MAIN_L2GETH with $MAIN_L2GETH"
-sed -i "s#MAIN_GETH#$MAIN_L2GETH#g" /etc/proxyd/proxyd.toml
+sed -i "s#MAIN_L2GETH#$MAIN_L2GETH#g" /etc/proxyd/proxyd.toml
 echo "Replacing REPLICA_L2GETH with $REPLICA_L2GETH"
 sed -i "s#REPLICA_L2GETH#$REPLICA_L2GETH#g" /etc/proxyd/proxyd.toml
 echo "Replacing ELASTICACHE with $ELASTICACHE"
