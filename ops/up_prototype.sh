bfea7ec27f81f7b2fe9d3d62a61f0d0f7745138f
Michael Montour <mmontour@enya.ai>
2022-01-12 22:31:19 -0800

    Fixup  Changes to be committed: 	modified:   .gitignore 	new file:   ops/up_prototype.sh

---
+++ ops/up_prototype.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Script to start up a local V3 prototype system.
+#
+# NOTE - the deployer steps are very slow because the L1 chain operates
+# with a fixed block time rather than auto-mining a block per transaction.
+# The deployers could be re-written to do more stuff in parallel. For now,
+# schedule a coffee break.
+set -x
+
+#Build if requested
+if [[ $BUILD == 1 ]]; then
+  yarn
+  yarn build
+  docker-compose build
+fi
+
+docker-compose up -d l1_chain dtl
+sleep 10
+echo "Starting Optimism deployer"
+
+docker-compose up deployer
+docker-compose up -d l2geth batch_submitter relayer
+sleep 10
+echo "Starting Boba deployer (slow)"
+docker-compose up boba_deployer
+docker-compose up -d boba_message-relayer-fast gas_oracle
+sleep 10
+echo "Base system is up"
+(cd ../packages/boba/v3-prototype ; yarn deploy)
+echo "Activated V3 contracts, starting agents"
+docker-compose up -d l1-agent l2-agent
+echo "All containers started"
+sleep 5
+docker-compose logs --follow
