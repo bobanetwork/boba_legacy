9096436677986db2f95478598891baf07d77615b
cby3149 <cby3149@gmail.com>
2022-01-13 20:16:53 -0800

    Add L1 security fee estimation

--- packages/boba/gateway/src/components/mainMenu/gasSwitcher/GasSwitcher.js
+++ packages/boba/gateway/src/components/mainMenu/gasSwitcher/GasSwitcher.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Box } from '@material-ui/system'
 import { useSelector } from 'react-redux'
 import * as S from './GasSwitcher.styles.js'
-import { ethers } from 'ethers'
 
 import { selectGas } from 'selectors/balanceSelector'
 import { selectVerifierStatus } from 'selectors/verifierSelector'
@@ -18,7 +17,6 @@ function GasSwitcher() {
 
   useEffect(() => {
     async function getGasSavings () {
-      const l2GasPrice = await networkService.L2Provider.getGasPrice();
       const l2GasEstimate = await networkService.L2Provider.estimateGas({
         from: '0x5E7a06025892d8Eef0b5fa263fA0d4d2E5C3B549',
         to: '0x17C83E2B96ACfb5190d63F5E46d93c107eC0b514',
@@ -26,21 +24,7 @@ function GasSwitcher() {
         data:
           '0x7ff36ab5000000000000000000000000000000000000000000000000132cc41aecbfbace00000000000000000000000000000000000000000000000000000000000000800000000000000000000000005e7a06025892d8eef0b5fa263fa0d4d2e5c3b54900000000000000000000000000000000000000000000000000000001c73d14500000000000000000000000000000000000000000000000000000000000000002000000000000000000000000deaddeaddeaddeaddeaddeaddeaddeaddead00000000000000000000000000005008f837883ea9a07271a1b5eb0658404f5a9610',
       });
-      const l1GasCost = await networkService.gasOralceContract.getL1Fee(
-        ethers.utils.serializeTransaction({
-          to: '0x17C83E2B96ACfb5190d63F5E46d93c107eC0b514',
-          value: '0x38d7ea4c68000',
-          data:
-            '0x7ff36ab5000000000000000000000000000000000000000000000000132cc41aecbfbace00000000000000000000000000000000000000000000000000000000000000800000000000000000000000005e7a06025892d8eef0b5fa263fa0d4d2e5c3b54900000000000000000000000000000000000000000000000000000001c73d14500000000000000000000000000000000000000000000000000000000000000002000000000000000000000000deaddeaddeaddeaddeaddeaddeaddeaddead00000000000000000000000000005008f837883ea9a07271a1b5eb0658404f5a9610',
-        })
-      );
-      const totalGasCostWei = l2GasPrice.mul(l2GasEstimate).add(l1GasCost).toNumber();
-      console.log({
-        gasL1: gas.gasL1,
-        l2GasEstimate: l2GasEstimate.toNumber(),
-        gasL2: gas.gasL2,
-        totalGasCostWei: totalGasCostWei,
-      })
+      const totalGasCostWei = await networkService.estimateL1SecurityFee()
       const gasSavings = Number(gas.gasL1) * l2GasEstimate.toNumber() / (Number(gas.gasL2) + totalGasCostWei / Math.pow(10, 9));
       setSavings(gasSavings ? gasSavings : 0);
       return gasSavings
