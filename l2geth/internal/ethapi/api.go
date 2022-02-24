550281ed3750c72a884ac43be3e69b8567aa5470
Boyuan-Chen <46272347+Boyuan-Chen@users.noreply.github.com>
2022-02-24 15:12:25 -0800

    [L2Geth] Add Boba gas fee hard fork (#422)
    * Add Boba gas hard fork
    
    * Add Mainnet hard fork and fix gas estimation
    
    * Update mainnet gas fork
    
    Co-authored-by: CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
    (cherry picked from commit 0004ea17d9cfb3f25a846c40f2ff6653b921f0c0)

--- l2geth/internal/ethapi/api.go
+++ l2geth/internal/ethapi/api.go
@@ -974,9 +974,10 @@ func (s *PublicBlockChainAPI) Call(ctx context.Context, args CallArgs, blockNrOr
 func DoEstimateGas(ctx context.Context, b Backend, args CallArgs, blockNrOrHash rpc.BlockNumberOrHash, gasCap *big.Int) (hexutil.Uint64, error) {
 	// Binary search the gas requirement, as it may be higher than the amount used
 	var (
-		lo  uint64 = params.TxGas - 1
-		hi  uint64
-		cap uint64
+		lo          uint64 = params.TxGas - 1
+		hi          uint64
+		cap         uint64
+		isGasUpdate bool = true
 	)
 	if args.Gas != nil && uint64(*args.Gas) >= params.TxGas {
 		hi = uint64(*args.Gas)
@@ -1043,6 +1044,16 @@ func DoEstimateGas(ctx context.Context, b Backend, args CallArgs, blockNrOrHash 
 			}
 			return 0, fmt.Errorf("gas required exceeds allowance (%d)", cap)
 		}
+	}
+
+	block, err := b.BlockByNumberOrHash(ctx, blockNrOrHash)
+	if err == nil {
+		blockNr := block.Number()
+		isGasUpdate = b.ChainConfig().IsGasUpdate(big.NewInt(blockNr.Int64()))
+	}
+
+	if !isGasUpdate {
+		return hexutil.Uint64(hi), nil
 	}
 
 	nonce, _ := b.GetPoolNonce(ctx, *args.From)
