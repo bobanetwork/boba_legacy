ba90abf185d3214408e59c4d6d81506912b000f0
cby3149 <cby3149@gmail.com>
2022-03-03 15:31:00 -0800

    Update Rinkeby contract addresses

--- packages/boba/contracts/contracts/standards/xL2GovernanceERC20.sol
+++ packages/boba/contracts/contracts/standards/xL2GovernanceERC20.sol
@@ -4,8 +4,8 @@ pragma solidity >0.7.5;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import { ERC20Votes } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import { ERC20VotesComp } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
+import { ERC20Votes } from "@openzeppelin/contracts/token/ERC20/extensions/regenesis/ERC20VotesRegenesis.sol";
+import { ERC20VotesComp } from "@openzeppelin/contracts/token/ERC20/extensions/regenesis/ERC20VotesCompRegenesis.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 /* External Imports */
