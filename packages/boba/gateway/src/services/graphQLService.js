f540e509d1fb83f0fc3c2d712968b3b66d6182d1
wsdt <kevin.riedl.privat@gmail.com>
2022-03-28 21:17:55 +0200

    further fixes: GraphQL Service

--- packages/boba/gateway/src/services/graphQLService.js
+++ packages/boba/gateway/src/services/graphQLService.js
@@ -1,35 +1,29 @@
-import axios from 'axios'
+const axios = require('axios')
 
 require('dotenv').config()
 
 class GraphQLService {
 
   getBridgeEndpoint = () => {
-    return `https://graph.${process.env.REACT_APP_CHAIN ?? 'rinkeby'}.boba.network/subgraphs/name/boba/Bridges/graphql`
+    return `https://graph.${process.env.REACT_APP_CHAIN ?? 'rinkeby'}.boba.network:8000/subgraphs/name/boba/Bridges/graphql`
   }
 
   async queryBridgeProposalCreated() {
+    const query = {
+      // operationName: 'proposalCreated',
+      query: `query proposalCreated { governorProposalCreateds { proposalId values description proposer } }`,
+      variables: {},
+    }
 
-    const query = `
-          {
-            governorProposalCreateds {
-              proposalId
-              values
-              description
-              proposer
-            }
-          }
-      `
-
-    return await axios({
-      url: this.getBridgeEndpoint,
+    return axios({
+      url: this.getBridgeEndpoint(),
       method: 'post',
-      data: {
-        query,
-      },
+      headers: {"Content-Type": "application/json", "Accept": "application/json"},
+      data: JSON.stringify(query),
     })
   }
 }
 
 const graphQLService = new GraphQLService();
 export default graphQLService;
+
