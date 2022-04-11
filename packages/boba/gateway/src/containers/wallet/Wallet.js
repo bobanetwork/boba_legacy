0dee513c9dcee1f61a22016edc4a7d7fe4dbcfb9
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2022-04-11 12:29:29 -0700

    Gateway addmetaswap support (#91)
    * add metatransaction
    
    * test metatx

--- packages/boba/gateway/src/containers/wallet/Wallet.js
+++ packages/boba/gateway/src/containers/wallet/Wallet.js
@@ -136,10 +136,13 @@ function Wallet() {
     if (res) dispatch(openAlert('Emergency Swap submitted'))
   }
 
+// disable hisding the EMERGENCY SWAP for testing
+// && tooSmallETH
+
   return (
     <S.PageContainer>
       <PageTitle title="Wallet" />
-      {layer === 'L2' && tooSmallETH &&
+      {layer === 'L2'  &&
         <S.LayerAlert>
           <S.AlertInfo>
             <AlertIcon />
@@ -147,7 +150,7 @@ function Wallet() {
               variant="body2"
               component="p"
             >
-              <span style={{opacity: '1.0'}}>WARNING: Low ETH balance</span>.
+              <span style={{opacity: '1.0'}}>NOTE: ETH balance</span>.
               {' '}
               <span style={{opacity: '0.6'}}>Using Boba requires a minimum ETH balance (of 0.002 ETH)
               regardless of your fee setting, otherwise MetaMask may incorrectly reject transactions.
