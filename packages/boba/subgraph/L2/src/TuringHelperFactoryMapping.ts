c910a968b0d6121f79f0ea73afef06e8ff2c422f
wsdt <kevin.riedl.privat@gmail.com>
2022-04-20 20:13:35 +0200

    subgraph integration

--- packages/boba/subgraph/L2/src/TuringHelperFactoryMapping.ts
+++ packages/boba/subgraph/L2/src/TuringHelperFactoryMapping.ts
@@ -4,10 +4,11 @@ import { TuringHelperDeployedEvent } from '../generated/schema'
 export function handleTuringHelperDeployed(
   event: TuringHelperDeployed
 ): void {
-  let transactionHash: string = event.transaction.hash.toHex()
-  let transfer = new TuringHelperDeployedEvent(transactionHash)
-  transfer.owner = event.params.owner
-  transfer.proxy = event.params.proxy
-  transfer.depositedBoba = event.params.depositedBoba.toString()
-  transfer.save()
+  let id: string = event.transaction.hash.toHex()
+  let helperDeployed = new TuringHelperDeployedEvent(id)
+  helperDeployed.id = id
+  helperDeployed.owner = event.params.owner
+  helperDeployed.proxy = event.params.proxy
+  helperDeployed.depositedBoba = event.params.depositedBoba.toString()
+  helperDeployed.save()
 }
