3f37d44d385cb507404ea7ef4bf0e2fb61c20470
Sahil Kashetwar <sahil@enya.ai>
2022-05-03 10:16:47 +0530

    removed tokenlist file

--- packages/boba/gateway/src/containers/bridge/Bridge.styles.js
+++ packages/boba/gateway/src/containers/bridge/Bridge.styles.js
@@ -1,7 +1,5 @@
 import styled from '@emotion/styled'
 import { Box } from "@mui/material"
-import bobaGlassBg from 'images/boba2/boba_glass_bg.svg'
-import bobaGlassIcon from 'images/boba2/boba_glass.svg'
 
 export const PageContainer = styled(Box)(({ theme }) => ({
   margin: '20px auto',
@@ -57,27 +55,6 @@ export const TitleContainer = styled(Box)(({ theme }) => ({
     '::before': {
       display: 'none'
     }
-  },
-  '::before': {
-    content: '" "',
-    position: 'absolute',
-    top: '5%',
-    right: '25%',
-    width: '50px',
-    height: '50px',
-    background: `url(${bobaGlassIcon}) no-repeat`,
-    backgroundSize: '100% 90%',
-  },
-  '::after': {
-    content: '" "',
-    position: 'absolute',
-    bottom: '20%',
-    left: 0,
-    width: '90%',
-    height: '100%',
-    background: `url(${bobaGlassBg}) no-repeat`,
-    backgroundSize: '100%',
-    zIndex: '-1'
   }
 }))
 
