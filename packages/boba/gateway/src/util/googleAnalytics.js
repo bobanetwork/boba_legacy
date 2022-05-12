084b7750c3b7ec8351cfbd0146a998dd05b3f169
Sahil Kashetwar <sahil@enya.ai>
2022-05-12 09:23:21 +0530

    removed console

--- packages/boba/gateway/src/util/googleAnalytics.js
+++ packages/boba/gateway/src/util/googleAnalytics.js
@@ -1,9 +1,7 @@
 import ReactGA from "react-ga4"
 
 export const initGa = () => {
-  console.log(`Initing google analytics...`);
   ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID);
-  console.log(`GA 4 initiated`);
 }
 
 export const trackPageView = (pageName) => {
