5243917d4e2f106268c1b0e13aa69f26f52ddb60
wsdt <kevin.riedl.privat@gmail.com>
2022-07-04 16:31:18 +0200

    faucet

--- boba_community/turing-twitter/hardhat.config.ts
+++ boba_community/turing-twitter/hardhat.config.ts
@@ -12,6 +12,10 @@ const config: HardhatUserConfig = {
     boba_local: {
       url: 'http://localhost:8545',
     },
+    boba_opera_testnet: {
+      url: 'https://testnet.bobaopera.boba.network',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     boba_rinkeby: {
       url: 'https://rinkeby.boba.network',
       bridgeCounterpartUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161', // public RPC
