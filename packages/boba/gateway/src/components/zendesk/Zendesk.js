31974722baa1a1d7210c077661078f4605e273d6
Sahil Kashetwar <sahil@enya.ai>
2022-07-18 13:21:54 +0530

    cleanup

--- packages/boba/gateway/src/components/zendesk/Zendesk.js
+++ packages/boba/gateway/src/components/zendesk/Zendesk.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import ReactZendesk, { ZendeskAPI } from "react-zendesk";
+import React from 'react';
+import ReactZendesk from "react-zendesk";
 
 const setting = {
   color: {
