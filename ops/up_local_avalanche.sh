bde3f64000d34bc8d2558db6eb62f344854542ba
cby3149 <cby3149@gmail.com>
2022-07-27 21:03:18 -0700

    Fix integration tests

--- ops/up_local_avalanche.sh
+++ ops/up_local_avalanche.sh
@@ -41,13 +41,9 @@ DOCKERFILE="docker-compose-avalanche.yml"
 if [[ $BUILD == 1 ]]; then
   docker-compose -f docker-compose-avalanche.yml build --parallel -- builder l2geth l1_chain
   docker-compose -f docker-compose-avalanche.yml build --parallel -- deployer dtl batch_submitter relayer integration_tests
-  docker-compose -f docker-compose-avalanche.yml build -- boba_message-relayer-fast
-  docker-compose -f docker-compose-avalanche.yml build -- gas_oracle
-  docker-compose -f docker-compose-avalanche.yml build -- boba_deployer
-  docker-compose -f docker-compose-avalanche.yml build -- fraud-detector
-  docker-compose -f docker-compose-avalanche.yml build -- monitor
-  docker-compose -f docker-compose-avalanche.yml build -- verifier
-  docker-compose -f docker-compose-avalanche.yml build -- replica
+  docker-compose -f docker-compose-avalanche.yml build --parallel -- boba_message-relayer-fast gas_oracle boba_deployer
+  docker-compose -f docker-compose-avalanche.yml build --parallel -- fraud-detector monitor
+  docker-compose -f docker-compose-avalanche.yml build --parallel -- verifier replica
 elif [[ $BUILD == 0 ]]; then
   docker-compose -f $DIR/$DOCKERFILE pull
   echo 1
