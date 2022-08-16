3b68db59bcfb084ee5f1040b758c238e12746c6a
cby3149 <cby3149@gmail.com>
2022-08-16 16:32:07 -0400

    Disable google analytics for multi-chain

--- packages/boba/gateway/src/util/googleAnalytics.js
+++ packages/boba/gateway/src/util/googleAnalytics.js
@@ -1,11 +1,11 @@
 import ReactGA from "react-ga4"
 
 export const initGa = () => {
-  if (window.location.hostname === 'localhost') {
-    return null;
-  }
+  // if (window.location.hostname === 'localhost') {
+  return null;
+  // }
 
-  ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID);
+  // ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID);
 }
 
 export const trackPageView = (pageName) => {
