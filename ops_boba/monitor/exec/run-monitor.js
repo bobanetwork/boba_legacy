97e1c32c49c455cfbd8a3e820dc6e5a71b514f95
Trang Tran <trang@enya.ai>
2022-08-31 14:38:48 +0700

    only monitor current chain

--- ops_boba/monitor/exec/run-monitor.js
+++ ops_boba/monitor/exec/run-monitor.js
@@ -47,6 +47,7 @@ const main = async () => {
   const exitMonitorService = require('../services/exitMonitor')
   const l1BridgeMonitorService = require('../services/l1BridgeMonitor')
   const messageMonitorService = require('../services/messageMonitor')
+  const LayerZeroBridgeMonitor = require('../services/layerZeroBridge')
 
   // l1 message monitor
   const messageService = new messageMonitorService()
@@ -80,6 +81,11 @@ const main = async () => {
 
   loop(() => blockService.startTransactionMonitor()).catch()
   loop(() => blockService.startCrossDomainMessageMonitor()).catch()
+
+  // monitor layerZero bridge:
+  const layerZeroBridgeMonitor = new LayerZeroBridgeMonitor()
+  await layerZeroBridgeMonitor.initScan()
+  await layerZeroBridgeMonitor.startMonitor()
 
   // enable the periodic transaction and check on Mainnet
   if (process.env.STAGE === 'mainnet') {
@@ -117,9 +123,9 @@ const main = async () => {
   }
 }
 
-;(async () => {
-  main().catch()
-})().catch((err) => {
-  console.log(err)
-  process.exit(1)
-})
+  ; (async () => {
+    main().catch()
+  })().catch((err) => {
+    console.log(err)
+    process.exit(1)
+  })
