8a432805d5cf5c20c6c1418180100c88cdbb9d0a
Trang Tran <trang@enya.ai>
2022-09-06 00:11:29 +0700

    Update table name

--- ops_boba/monitor/docker/mysql/1-init-database.sql
+++ ops_boba/monitor/docker/mysql/1-init-database.sql
@@ -1,8 +1,8 @@
 ALTER USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 
-CREATE DATABASE IF NOT EXISTS `omg_xv1`;
-CREATE DATABASE IF NOT EXISTS `omg_tx`;
-CREATE DATABASE IF NOT EXISTS `omg_receipts`;
-GRANT ALL ON `omg_xv1`.* TO 'admin'@'%';
-GRANT ALL ON `omg_tx`.* TO 'admin'@'%';
-GRANT ALL ON `omg_receipts`.* TO 'admin'@'%';
+CREATE DATABASE IF NOT EXISTS `boba_xv1`;
+CREATE DATABASE IF NOT EXISTS `boba_tx`;
+CREATE DATABASE IF NOT EXISTS `boba_receipts`;
+GRANT ALL ON `boba_xv1`.* TO 'admin'@'%';
+GRANT ALL ON `boba_tx`.* TO 'admin'@'%';
+GRANT ALL ON `boba_receipts`.* TO 'admin'@'%';
