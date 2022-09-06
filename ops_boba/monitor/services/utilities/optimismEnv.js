b9c6d360a1cb107dd18feafce1c4b27d164e3964
trangtran-enya <83643565+trangtran-enya@users.noreply.github.com>
2022-09-06 20:42:22 +0700

    Layer Zero monitor (#337)
    * cherry pick
    
    * Add AltL1 listen
    
    * only monitor current chain
    
    * Change to yarn try
    
    * Add layer zero addresses
    
    * Adjust with new addresss file
    
    * Update table name
    
    Co-authored-by: cby3149 <cby3149@gmail.com>
    (cherry picked from commit 572be9b31e1987201625f5c399827e15eeca6178)

--- ops_boba/monitor/services/utilities/optimismEnv.js
+++ ops_boba/monitor/services/utilities/optimismEnv.js
@@ -81,6 +81,12 @@ const OVM_L2_CROSS_DOMAIN_MESSENGER =
 
 const L1_BLOCK_CONFIRMATION = env.L1_BLOCK_CONFIRMATION || 0
 
+// layerZero env
+const LAYER_ZERO_ENABLE_TEST = Boolean(env.LAYER_ZERO_ENABLE_TEST) || true
+const LAYER_ZERO_CHAIN = env.LAYER_ZERO_CHAIN || 'Testnet'
+const LAYER_ZERO_BRIDGE = env.LAYER_ZERO_BRIDGE || 'EthBridgeToAvalanche'
+const LAYER_ZERO_LATEST_BLOCK = Number(env.LAYER_ZERO_LATEST_BLOCK) || 0
+
 class OptimismEnv {
   constructor() {
     this.logger = new Logger({ name: this.name })
@@ -163,6 +169,13 @@ class OptimismEnv {
     this.watcher = null
 
     this.l1BlockConfirmation = L1_BLOCK_CONFIRMATION
+
+    this.sequencerPublishWindow = 0
+
+    this.layerZeroEnableTest = LAYER_ZERO_ENABLE_TEST
+    this.layerZeroChain = LAYER_ZERO_CHAIN
+    this.layerZeroBridge = LAYER_ZERO_BRIDGE
+    this.layerZeroLatestBlock = LAYER_ZERO_LATEST_BLOCK
   }
 
   async initOptimismEnv() {
