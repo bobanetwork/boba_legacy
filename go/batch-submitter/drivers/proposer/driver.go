b76b0fd6b4d9762a1a94b7ebcb3289c39c7df5b2
cby3149 <cby3149@gmail.com>
2022-09-07 16:11:42 -0400

    Add more logs

--- go/batch-submitter/drivers/proposer/driver.go
+++ go/batch-submitter/drivers/proposer/driver.go
@@ -189,6 +189,8 @@ func (d *Driver) CraftBatchTx(
 	d.metrics.NumElementsPerBatch.Observe(float64(len(stateRoots)))
 
 	log.Info(name+" batch constructed", "num_state_roots", len(stateRoots))
+	log.Info(name+" batch constructed", "keyID", d.cfg.KeyId)
+	log.Info(name+" batch constructed", "ChainID", d.cfg.ChainID)
 
 	opts, err := ethawskmssigner.NewAwsKmsTransactorWithChainID(&d.cfg.KMS, d.cfg.KeyId, d.cfg.ChainID)
 
