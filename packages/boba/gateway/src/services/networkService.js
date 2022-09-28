a575ed8118f42d0a19ad632f7c5f38db9974ad0c
Sahil Kashetwar <sahil@enya.ai>
2022-09-28 11:38:24 +0530

    Adding the gasLimit for castingVOte on proposal

--- packages/boba/gateway/src/services/networkService.js
+++ packages/boba/gateway/src/services/networkService.js
@@ -4257,9 +4257,10 @@ class NetworkService {
         .connect(this.provider.getSigner())
         .attach(allAddresses.GovernorBravoDelegator)
 
-      return delegateCheck.castVote(id, userVote, tokenIds)
+      const res = await delegateCheck.castVote(id, userVote, tokenIds, { gasLimit: 3000000 })
+      return res;
 
-    } catch(error) {
+    } catch (error) {
       console.log("NS: castProposalVote error:",error)
       return error
     }
