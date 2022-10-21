5f1d0f2e941cba09ae721ae688e004ef7e9061ff
Souradeep Das <dsouradeep2@gmail.com>
2022-10-21 23:27:56 +0530

    fix: revert nft for failing mint (#419)
    * revert nft for failing mint
    
    * update tests
    
    * update README
    
    * add timeout
    
    * Apply suggestions from code review
    
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>
    
    * fix: flaky test
    
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>

---
+++ integration-tests/contracts/TestFailingMintL2StandardERC721.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.5;
+
+import "@boba/contracts/contracts/standards/L2StandardERC721.sol";
+
+/**
+* A Failing mint L2ERC721 contract
+*/
+contract TestFailingMintL2StandardERC721 is L2StandardERC721 {
+    /**
+     * @param _l2Bridge Address of the L2 standard bridge.
+     * @param _l1Contract Address of the corresponding L1 NFT contract.
+     * @param _name ERC721 name.
+     * @param _symbol ERC721 symbol.
+     */
+    constructor(
+        address _l2Bridge,
+        address _l1Contract,
+        string memory _name,
+        string memory _symbol,
+        string memory _baseTokenURI
+    )
+        L2StandardERC721(_l2Bridge, _l1Contract, _name, _symbol, _baseTokenURI) {}
+
+    function mint(address _to, uint256 _tokenId, bytes memory _data) public virtual override onlyL2Bridge {
+        revert("mint failing");
+    }
+}
