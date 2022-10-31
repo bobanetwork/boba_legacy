83c9ba713816f92dce596813d36e8ec7c0919a5d
Shijiang Guo <guoshijiang2012@163.com>
2022-10-31 20:57:12 +0800

    change log to github.com/ethereum/go-ethereum/log

--- go/batch-submitter/drivers/proposer/driver.go
+++ go/batch-submitter/drivers/proposer/driver.go
@@ -14,7 +14,7 @@ import (
 	"github.com/ethereum-optimism/optimism/go/bss-core/metrics"
 	"github.com/ethereum-optimism/optimism/go/bss-core/txmgr"
 	l2ethclient "github.com/ethereum-optimism/optimism/l2geth/ethclient"
-	"github.com/ethereum-optimism/optimism/l2geth/log"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
