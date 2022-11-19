b7ac3aa498a7498092c9a853cdb5c01de54486fc
Boyuan-Chen <46272347+Boyuan-Chen@users.noreply.github.com>
2022-11-19 12:02:03 -0800

    Deploy BOBA to Goerli (#441)
    * Deploy core contracts
    
    * Add boba core contract deployment
    
    * Deploy more contracts
    
    * Update gateway
    
    * Add contracts and fix gateway
    
    * Update contract addresses
    
    * Update SDK
    
    * Fix tests
    
    * Add default value to goerli
    
    * small docs and example fix for replica
    
    * Add L2StandardTokenFactory
    
    Co-authored-by: Ino Murko <2582555+InoMurko@users.noreply.github.com>

--- packages/boba/gateway/src/services/graphQLService.js
+++ packages/boba/gateway/src/services/graphQLService.js
@@ -7,8 +7,8 @@ class GraphQLService {
   getBridgeEndpoint = () => {
     if (APP_CHAIN === 'mainnet') {
       return `https://api.thegraph.com/subgraphs/name/bobanetwork/boba-l2-subgraph`
-    } else if (APP_CHAIN === 'rinkeby') {
-      return `https://graph.rinkeby.boba.network/subgraphs/name/boba/Bridges`
+    } else if (APP_CHAIN === 'goerli') {
+      return `https://graph.goerli.boba.network/subgraphs/name/boba/Bridges`
     } else {
       return ''
     }
@@ -22,7 +22,7 @@ class GraphQLService {
     curl -g -X POST \
     -H "Content-Type: application/json" \
     -d '{"query":"{ governorProposalCreateds {proposalId values description proposer}}"}' \
-    https://graph.rinkeby.boba.network/subgraphs/name/boba/Bridges
+    https://graph.goerli.boba.network/subgraphs/name/boba/Bridges
 
     curl -g -X POST \
     -H "Content-Type: application/json" \
@@ -58,7 +58,7 @@ class GraphQLService {
     curl -g -X POST \
       -H "Content-Type: application/json" \
       -d '{"query":"{ turingMonstersTransferEvents { to from tokenId }}"}' \
-      https://graph.rinkeby.boba.network/subgraphs/name/boba/Bridges
+      https://graph.goerli.boba.network/subgraphs/name/boba/Bridges
 
     curl -g -X POST \
       -H "Content-Type: application/json" \
