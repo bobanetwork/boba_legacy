185f6d72f213a7d1e1a409bd8edc90f7ac215d83
Sahil Kashetwar <sahil@enya.ai>
2022-11-22 02:13:12 +0530

    fix for import

--- packages/boba/gateway/src/containers/VoteAndDao/Dao/OldDao.js
+++ packages/boba/gateway/src/containers/VoteAndDao/Dao/OldDao.js
@@ -10,7 +10,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { Box, Typography } from '@mui/material'
