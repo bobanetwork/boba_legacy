4b486dcb7601a14a0a1e1571c1946491b93833ce
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2022-12-03 10:27:41 +0100

    resolve all dependency issues and bump tests

--- packages/contracts/test/contracts/L1/messaging/deposit.gas.spec.ts
+++ packages/contracts/test/contracts/L1/messaging/deposit.gas.spec.ts
@@ -1,20 +1,17 @@
-/* External Imports */
 import { ethers } from 'hardhat'
-import { Signer, ContractFactory, Contract } from 'ethers'
+import { Contract } from 'ethers'
+import { MockContract, smock } from '@defi-wonderland/smock'
 import { expectApprox } from '@eth-optimism/core-utils'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
-/* Internal Imports */
 import {
   deploy,
-  makeAddressManager,
   L2_GAS_DISCOUNT_DIVISOR,
   ENQUEUE_GAS_COST,
   NON_ZERO_ADDRESS,
   NON_NULL_BYTES32,
 } from '../../../helpers'
 import { expect } from '../../../setup'
-import { MockContract, smock } from '@defi-wonderland/smock'
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 // Still have some duplication from CanonicalTransactionChain.spec.ts, but it's so minimal that
 // this is probably cleaner for now. Particularly since we're planning to move all of this out into
@@ -31,36 +28,26 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
   })
 
   let AddressManager: Contract
-  before('Deploy address manager and register sequencer', async () => {
-    AddressManager = await makeAddressManager()
+  let CanonicalTransactionChain: Contract
+  before(async () => {
+    AddressManager = await deploy('Lib_AddressManager')
+
+    CanonicalTransactionChain = await deploy('CanonicalTransactionChain', {
+      args: [
+        AddressManager.address,
+        MAX_GAS_LIMIT,
+        L2_GAS_DISCOUNT_DIVISOR,
+        ENQUEUE_GAS_COST,
+      ],
+    })
+
+    const batches = await deploy('ChainStorageContainer', {
+      args: [AddressManager.address, 'CanonicalTransactionChain'],
+    })
+
     await AddressManager.setAddress(
       'OVM_Sequencer',
       await sequencer.getAddress()
-    )
-  })
-
-  let CanonicalTransactionChain: Contract
-  let Factory__ChainStorageContainer: ContractFactory
-  before('Init CTC and Storage Container contracts.', async () => {
-    CanonicalTransactionChain = await (
-      await ethers.getContractFactory('CanonicalTransactionChain')
-    ).deploy(
-      AddressManager.address,
-      MAX_GAS_LIMIT,
-      L2_GAS_DISCOUNT_DIVISOR,
-      ENQUEUE_GAS_COST
-    )
-
-    Factory__ChainStorageContainer = await ethers.getContractFactory(
-      'ChainStorageContainer'
-    )
-    const batches = await Factory__ChainStorageContainer.deploy(
-      AddressManager.address,
-      'CanonicalTransactionChain'
-    )
-    await Factory__ChainStorageContainer.deploy(
-      AddressManager.address,
-      'CanonicalTransactionChain'
     )
 
     await AddressManager.setAddress(
@@ -74,26 +61,24 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
     )
   })
 
-  // 3 Messenger
   let L1CrossDomainMessenger: Contract
-  before('Deploy Messenger proxy and implementation', async () => {
-    // Deploy the implementation contract first
-    const xDomainMessengerImpl = await (
-      await ethers.getContractFactory('L1CrossDomainMessenger')
-    ).deploy()
+  before(async () => {
+    const xDomainMessengerImpl = await deploy('L1CrossDomainMessenger')
+
     await AddressManager.setAddress(
       'L1CrossDomainMessenger',
       xDomainMessengerImpl.address
     )
-    // Deploy and initialize the Proxy Messenger
-    const proxy = await (
-      await ethers.getContractFactory('Lib_ResolvedDelegateProxy')
-    ).deploy(AddressManager.address, 'L1CrossDomainMessenger')
+
+    const proxy = await deploy('Lib_ResolvedDelegateProxy', {
+      args: [AddressManager.address, 'L1CrossDomainMessenger'],
+    })
+
     L1CrossDomainMessenger = xDomainMessengerImpl.attach(proxy.address)
+
     await L1CrossDomainMessenger.initialize(AddressManager.address)
   })
 
-  // 4 Bridge
   let L1ERC20: MockContract<Contract>
   let L1StandardBridge: Contract
   before('Deploy the bridge and setup the token', async () => {
@@ -112,6 +97,7 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
 
   describe('[GAS BENCHMARK] L1 to L2 Deposit costs [ @skip-on-coverage ]', async () => {
     const depositAmount = 1_000
+
     before(async () => {
       // Load a transaction into the queue first to 'dirty' the buffer's length slot
       await CanonicalTransactionChain.enqueue(
@@ -120,6 +106,7 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
         '0x1234'
       )
     })
+
     it('cost to deposit ETH', async () => {
       // Alice calls deposit on the bridge and the L1 bridge calls transferFrom on the token.
       const res = await L1StandardBridge.connect(alice).depositETH(
@@ -133,12 +120,14 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
       const receipt = await res.wait()
       const gasUsed = receipt.gasUsed.toNumber()
       console.log('    - Gas used:', gasUsed)
-      expectApprox(gasUsed, 160_424, {
+      //our deposits are a big expensier because we're updating deposit hash
+      expectApprox(gasUsed, 179_424, {
         absoluteUpperDeviation: 500,
         // Assert a lower bound of 1% reduction on gas cost. If your tests are breaking because your
         // contracts are too efficient, consider updating the target value!
         percentLowerDeviation: 1,
       })
+
       // Sanity check that the message was enqueued.
       expect(await CanonicalTransactionChain.getQueueLength()).to.equal(2)
     })
@@ -148,6 +137,7 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
         L1StandardBridge.address,
         depositAmount
       )
+
       // Alice calls deposit on the bridge and the L1 bridge calls transferFrom on the token.
       const res = await L1StandardBridge.connect(alice).depositERC20(
         L1ERC20.address,
@@ -156,10 +146,12 @@ describe('[GAS BENCHMARK] Depositing via the standard bridge [ @skip-on-coverage
         FINALIZATION_GAS,
         NON_NULL_BYTES32
       )
+
       const receipt = await res.wait()
       const gasUsed = receipt.gasUsed.toNumber()
       console.log('    - Gas used:', gasUsed)
-      expectApprox(gasUsed, 197_258, {
+
+      expectApprox(gasUsed, 225_558, {
         absoluteUpperDeviation: 500,
         // Assert a lower bound of 1% reduction on gas cost. If your tests are breaking because your
         // contracts are too efficient, consider updating the target value!
