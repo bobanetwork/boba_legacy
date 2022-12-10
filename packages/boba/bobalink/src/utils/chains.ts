a8b680c71f90f4ebd723f8cdae52c6f4e8fcc570
cby3149 <cby3149@gmail.com>
2022-12-09 17:51:52 -0800

    Update l2 contract addresses

--- packages/boba/bobalink/src/utils/chains.ts
+++ packages/boba/bobalink/src/utils/chains.ts
@@ -3,14 +3,14 @@ export const BobaLinkPairs = {
     '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419': {
       pair: 'ETH / USD',
       decimals: 8,
-      l2ContractAddress: '0x7C455414e0c0A5B13c9cCAbCB9DA9c477DfAdC27',
+      l2ContractAddress: '0x9e28dE704435871af476460B456Ec741fE5DE24f',
     },
   },
   2888: {
     '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e': {
       pair: 'ETH / USD',
       decimals: 8,
-      l2ContractAddress: '0x7C455414e0c0A5B13c9cCAbCB9DA9c477DfAdC27',
+      l2ContractAddress: '0x9e28dE704435871af476460B456Ec741fE5DE24f',
     },
   },
   31338: {
