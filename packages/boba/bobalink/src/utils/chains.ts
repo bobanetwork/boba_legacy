61cd2313502e03c7dba2b76a524dd218f7eb6e7f
cby3149 <cby3149@gmail.com>
2022-12-09 17:51:17 -0800

    Add contract addresses to bobalink

--- packages/boba/bobalink/src/utils/chains.ts
+++ packages/boba/bobalink/src/utils/chains.ts
@@ -1,31 +1,38 @@
 export const BobaLinkPairs = {
   288: {
-    '0x14e613AC84a31f709eadbdF89C6CC390fDc9540A': {
-      pair: 'BNB / USD',
-      decimals: 8,
-      l2ContractAddress: '0x0000000000000000000000000000000000000000',
-    },
     '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419': {
       pair: 'ETH / USD',
       decimals: 8,
-      l2ContractAddress: '0x0000000000000000000000000000000000000000',
-    },
-    '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c': {
-      pair: 'BTC / USD',
-      decimals: 8,
-      l2ContractAddress: '0x0000000000000000000000000000000000000000',
+      l2ContractAddress: '0x9e28dE704435871af476460B456Ec741fE5DE24f',
     },
   },
   2888: {
-    '0xA39434A63A52E749F02807ae27335515BA4b07F7': {
-      pair: 'BTC / USD',
-      decimals: 8,
-      l2ContractAddress: '0x0000000000000000000000000000000000000000',
-    },
     '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e': {
       pair: 'ETH / USD',
       decimals: 8,
-      l2ContractAddress: '0x0000000000000000000000000000000000000000',
+      l2ContractAddress: '0x9e28dE704435871af476460B456Ec741fE5DE24f',
+    },
+  },
+  1294: {
+    '0x9ce2388a1696e22F870341C3FC1E89710C7569B5': {
+      pair: 'ETH / USD',
+      decimals: 8,
+      l2ContractAddress: '0x9e28dE704435871af476460B456Ec741fE5DE24f',
+    },
+    '0x4497B606be93e773bbA5eaCFCb2ac5E2214220Eb': {
+      pair: 'GLMR / USD',
+      decimals: 8,
+      l2ContractAddress: '0x2dA3721dcd04A17195de07D21bCFCCBBFDF4Fc93',
+    },
+    '0x8c4425e141979c66423A83bE2ee59135864487Eb': {
+      pair: 'BTC / USD',
+      decimals: 8,
+      l2ContractAddress: '0x7590CdA8585150Bd1228497AA21D8365400c20cb',
+    },
+    '0xA122591F60115D63421f66F752EF9f6e0bc73abC': {
+      pair: 'USDC / USD',
+      decimals: 8,
+      l2ContractAddress: '0x9285eF61F09c2CAD195FEa8834C3E2bB8E442BFF',
     },
   },
 }
