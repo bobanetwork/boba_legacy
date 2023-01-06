03908583466f56a58ae83e37f053321125e570cd
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2023-01-06 13:20:25 +0100

    bump solidity compiler

--- packages/boba/bundler/packages/bundler/contracts/BundlerHelper.sol
+++ packages/boba/bundler/packages/bundler/contracts/BundlerHelper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.17;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "solidity-string-utils/StringUtils.sol";
