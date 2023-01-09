844c4efabe03c4d7258e60eb351d55487742c50f
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2023-01-09 09:13:49 +0100

    split the building stack steps
    extract vital builder building

---
+++ ops/scripts/build-ci.sh
@@ -0,0 +1,10 @@
+yarn
+yarn build
+
+docker-compose build --parallel -- builder l2geth l1_chain &&
+docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests &&
+docker-compose build --parallel -- boba_message-relayer-fast boba_deployer fraud-detector &&
+
+docker ps
+
+wait
