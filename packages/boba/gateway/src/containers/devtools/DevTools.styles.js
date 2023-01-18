fd09f8064cbf7425b432847ad1b044157d3e11f4
Sahil K <86316370+sk-enya@users.noreply.github.com>
2023-01-18 14:33:12 +0530

    [Gateway] :  Add Babylons ecosystem (#518)
    * add babylons in ecosystem page
    
    * console cleanup

--- packages/boba/gateway/src/containers/devtools/DevTools.styles.js
+++ packages/boba/gateway/src/containers/devtools/DevTools.styles.js
@@ -1,4 +1,4 @@
-import { Box, Divider, Grid, IconButton, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const PageContainer = styled(Box)(({ theme }) => ({
