5dd701737bb6b2aa49f87993aa8e1e2110ab5b74
Boyuan-Chen <46272347+Boyuan-Chen@users.noreply.github.com>
2023-01-19 02:54:04 -0800

    Add account monitor feature in monitor service and simplify code in GPO service (#510)
    * Simplify code
    
    * Add unified functions and fix bugs
    
    * Add unit tests
    
    * Add balance monitor and improve codebase
    
    * yarn conflict resolved
    
    * Remove unused packages and bump up hardhat version
    
    Co-authored-by: Ino Murko <2582555+InoMurko@users.noreply.github.com>
    (cherry picked from commit 7911b0e6c3fd8f87d4468b24c307e13074d1ee1b)

--- ops_boba/monitor/services/utils/globalEnv.js
+++ ops_boba/monitor/services/utils/globalEnv.js
@@ -3,8 +3,9 @@
 const ethers = require('ethers')
 const { CrossChainMessenger } = require('@eth-optimism/sdk')
 const { Logger } = require('@eth-optimism/common-ts')
-const Mutex = require('async-mutex').Mutex
 const fetch = require('node-fetch')
+
+const { removeBlankStringInArray } = require('./utils')
 
 const addressManagerJSON = require('@eth-optimism/contracts/artifacts/contracts/libraries/resolver/Lib_AddressManager.sol/Lib_AddressManager.json')
 const L1LiquidityPoolJson = require('@boba/contracts/artifacts/contracts/LP/L1LiquidityPool.sol/L1LiquidityPool.json')
@@ -14,6 +15,12 @@ const L2StandardBridgeJson = require('@eth-optimism/contracts/artifacts/contract
 const StateCommitmentChainJson = require('@eth-optimism/contracts/artifacts/contracts/L1/rollup/StateCommitmentChain.sol/StateCommitmentChain.json')
 
 require('dotenv').config()
+
+const second = 1000
+const minute = 60 * 1000
+const hour = 60 * 60 * 1000
+
+/* eslint-disable */
 const env = process.env
 const L1_NODE_WEB3_URL = env.L1_NODE_WEB3_URL || 'http://localhost:8545'
 const L2_NODE_WEB3_URL = env.L2_NODE_WEB3_URL || 'http://localhost:9545'
@@ -22,87 +29,61 @@ const MYSQL_HOST_URL = env.MYSQL_HOST_URL || '127.0.0.1'
 const MYSQL_PORT = env.MYSQL_PORT || 3306
 const MYSQL_USERNAME = env.MYSQL_USERNAME
 const MYSQL_PASSWORD = env.MYSQL_PASSWORD
-const MYSQL_DATABASE_NAME = env.MYSQL_DATABASE_NAME || 'OMGXV1'
-const MYSQL_DBNAME_TX = env.MYSQL_DBNAME_TX
-const MYSQL_DBNAME_RECEIPT = env.MYSQL_DBNAME_RECEIPT
-
-const ADDRESS_MANAGER_ADDRESS = env.ADDRESS_MANAGER_ADDRESS
-const L2_MESSENGER_ADDRESS =
-  env.L2_MESSENGER_ADDRESS || '0x4200000000000000000000000000000000000007'
-
-const TRANSACTION_MONITOR_INTERVAL =
-  env.TRANSACTION_MONITOR_INTERVAL || 3 * 1000
-const CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL =
-  env.CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL || 5 * 1000
-const STATE_ROOT_MONITOR_INTERVAL = env.STATE_ROOT_MONITOR_INTERVAL || 5 * 1000
-const EXIT_MONITOR_INTERVAL = env.EXIT_MONITOR_INTERVAL || 3 * 1000
-const L1_BRIDGE_MONITOR_INTERVAL = env.L1_BRIDGE_MONITOR_INTERVAL || 3 * 1000
-const MESSAGE_MONITOR_INTERVAL = env.MESSAGE_MONITOR_INTERVAL || 10 * 60 * 1000
-
-const SQL_DISCONNECTED = 'disconnected'
-
-const WHITELIST_SLEEP = 60 // in seconds
-const NON_WHITELIST_SLEEP = Number(env.NON_WHITELIST_SLEEP) || 10 * 60 // in seconds
-const WHITELIST = 'whitelist'
-const NON_WHITELIST = 'non_whitelist'
-
-const L2_RATE_LIMIT = 500
-const L2_SLEEP_THRESH = 100
-
-const FILTER_ENDPOINT = env.FILTER_ENDPOINT
-
-const STATE_ROOT_MONITOR_START_BLOCK = env.STATE_ROOT_MONITOR_START_BLOCK || 0
-const STATE_ROOT_MONITOR_LOG_INTERVAL =
-  env.STATE_ROOT_MONITOR_LOG_INTERVAL || 2000
+const MYSQL_DATABASE_NAME = env.MYSQL_DATABASE_NAME || 'BOBAV1'
+const MYSQL_DBNAME_TX = `${env.MYSQL_DATABASE_NAME}CDM`
 
 const BOBA_DEPLOYER_URL = env.BOBA_DEPLOYER_URL
+const FILTER_ENDPOINT = env.FILTER_ENDPOINT
+
+const ADDRESS_MANAGER_ADDRESS = env.ADDRESS_MANAGER_ADDRESS
+const L2_MESSENGER_ADDRESS = '0x4200000000000000000000000000000000000007'
+const OVM_L2_STANDARD_BRIDGE_ADDRESS = '0x4200000000000000000000000000000000000010'
 const L1LiquidityPoolAddress = env.PROXY__L1_LIQUIDITY_POOL_ADDRESS
 const L2LiquidityPoolAddress = env.PROXY__L2_LIQUIDITY_POOL_ADDRESS
 
-const EXIT_MONITOR_LOG_INTERVAL = env.STATE_ROOT_MONITOR_LOG_INTERVAL || 2000
+const TRANSACTION_MONITOR_INTERVAL = env.TRANSACTION_MONITOR_INTERVAL || 3 * second
+const CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL = env.CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL || 15 * minute
+const STATE_ROOT_MONITOR_INTERVAL = env.STATE_ROOT_MONITOR_INTERVAL || hour
+const EXIT_MONITOR_INTERVAL = env.EXIT_MONITOR_INTERVAL || 15 * minute
+const L1_BRIDGE_MONITOR_INTERVAL = env.L1_BRIDGE_MONITOR_INTERVAL || 3 * minute
 
+const STATE_ROOT_MONITOR_START_BLOCK = env.STATE_ROOT_MONITOR_START_BLOCK || 0
 const L1_BRIDGE_MONITOR_START_BLOCK = env.L1_BRIDGE_MONITOR_START_BLOCK || 0
-const L1_BRIDGE_MONITOR_LOG_INTERVAL =
-  env.L1_BRIDGE_MONITOR_LOG_INTERVAL || 2000
 
-// seconds
-const L1_CROSS_DOMAIN_MESSAGE_WAITING_TIME =
-  env.L1_CROSS_DOMAIN_MESSAGE_WAITING_TIME || 30
-const L2_CROSS_DOMAIN_MESSAGE_WAITING_TIME =
-  env.L2_CROSS_DOMAIN_MESSAGE_WAITING_TIME || 60
+const STATE_ROOT_MONITOR_LOG_INTERVAL = env.STATE_ROOT_MONITOR_LOG_INTERVAL || 2000
+const EXIT_MONITOR_LOG_INTERVAL = env.EXIT_MONITOR_LOG_INTERVAL || 2000
+const L1_BRIDGE_MONITOR_LOG_INTERVAL = env.L1_BRIDGE_MONITOR_LOG_INTERVAL || 2000
 
-const OVM_L2_STANDARD_BRIDGE_ADDRESS =
-  env.OVM_L2_STANDARD_BRIDGE_ADDRESS ||
-  '0x4200000000000000000000000000000000000010'
-const OVM_L2_CROSS_DOMAIN_MESSENGER =
-  '0x4200000000000000000000000000000000000007'
+const L1_CROSS_DOMAIN_MESSAGE_WAITING_TIME = env.L1_CROSS_DOMAIN_MESSAGE_WAITING_TIME || 300
+const L2_CROSS_DOMAIN_MESSAGE_WAITING_TIME = env.L2_CROSS_DOMAIN_MESSAGE_WAITING_TIME || 3600
 
 const L1_BLOCK_CONFIRMATION = env.L1_BLOCK_CONFIRMATION || 0
 
 const NUMBER_OF_BLOCKS_TO_FETCH = env.NUMBER_OF_BLOCKS_TO_FETCH || 10000000
 
-// layerZero env
-const LAYER_ZERO_ENABLE_TEST =
-  env.LAYER_ZERO_ENABLE_TEST === 'true' ? true : false
+const LAYER_ZERO_ENABLE_TEST = env.LAYER_ZERO_ENABLE_TEST === 'true' ? true : false
 const LAYER_ZERO_CHAIN = env.LAYER_ZERO_CHAIN || 'Testnet'
-const LAYER_ZERO_BRIDGES = env.LAYER_ZERO_BRIDGES || 'Proxy__EthBridgeToAvalanche'
+const LAYER_ZERO_BRIDGES = env.LAYER_ZERO_BRIDGES || ''
 const LAYER_ZERO_LATEST_BLOCK = Number(env.LAYER_ZERO_LATEST_BLOCK) || 0
 
-class OptimismEnv {
+const PERIODIC_TRANSACTION_PRIVATE_KEY = env.PERIODIC_TRANSACTION_PRIVATE_KEY
+const PERIODIC_TRANSACTION_INTERVAL = env.PERIODIC_TRANSACTION_INTERVAL || 10 * minute
+
+const BOBASTRAW_CONTACT_ADDRESSES = env.BOBASTRAW_CONTACT_ADDRESSES || ''
+const BOBASTRAW_MONITOR_INTERVAL = env.BOBASTRAW_MONITOR_INTERVAL || 10 * minute
+
+const L1_BALANCE_MONITOR_ADDRESSES = env.L1_BALANCE_MONITOR_ADDRESSES || ''
+const L2_BALANCE_MONITOR_ADDRESSES = env.L2_BALANCE_MONITOR_ADDRESSES || ''
+const BALANCE_MONITOR_INTERVAL = env.BALANCE_MONITOR_INTERVAL || 10 * minute
+/* eslint-enable */
+
+class GlobalEnv {
   constructor() {
+    /* eslint-disable */
     this.logger = new Logger({ name: this.name })
 
-    this.L1Provider = new ethers.providers.StaticJsonRpcProvider(
-      L1_NODE_WEB3_URL
-    )
-    // this.L1Provider.on('debug', (info) => {
-    //   if (info.action === 'request') {
-    //     this.logger.info('ethers', info.request)
-    //   }
-    // })
-    this.L2Provider = new ethers.providers.StaticJsonRpcProvider(
-      L2_NODE_WEB3_URL
-    )
+    this.L1Provider = new ethers.providers.StaticJsonRpcProvider(L1_NODE_WEB3_URL)
+    this.L2Provider = new ethers.providers.StaticJsonRpcProvider(L2_NODE_WEB3_URL)
 
     this.MySQLHostURL = MYSQL_HOST_URL
     this.MySQLPort = MYSQL_PORT
@@ -110,7 +91,6 @@ class OptimismEnv {
     this.MySQLPassword = MYSQL_PASSWORD
     this.MySQLDatabaseName = MYSQL_DATABASE_NAME
     this.MySQLDatabaseNameTx = MYSQL_DBNAME_TX
-    this.MySQLDatabaseNameReceipt = MYSQL_DBNAME_RECEIPT
 
     this.addressManagerAddress = ADDRESS_MANAGER_ADDRESS
     this.L1CrossDomainMessenger = null
@@ -120,28 +100,13 @@ class OptimismEnv {
 
     this.numberBlockToFetch = NUMBER_OF_BLOCKS_TO_FETCH
     this.transactionMonitorInterval = TRANSACTION_MONITOR_INTERVAL
-    this.crossDomainMessageMonitorInterval =
-      CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL
+    this.crossDomainMessageMonitorInterval = CROSS_DOMAIN_MESSAGE_MONITOR_INTERVAL
     this.stateRootMonitorInterval = STATE_ROOT_MONITOR_INTERVAL
     this.exitMonitorInterval = EXIT_MONITOR_INTERVAL
     this.l1BridgeMonitorInterval = L1_BRIDGE_MONITOR_INTERVAL
-    this.messageMonitorInterval = MESSAGE_MONITOR_INTERVAL
-
-    this.whitelistSleep = WHITELIST_SLEEP
-    this.nonWhitelistSleep = NON_WHITELIST_SLEEP
-    this.whitelistString = WHITELIST
-    this.nonWhitelistString = NON_WHITELIST
-
-    this.L2rateLimit = L2_RATE_LIMIT
-    this.L2sleepThresh = L2_SLEEP_THRESH
-
-    this.sqlDisconnected = SQL_DISCONNECTED
 
     this.transactionMonitorSQL = false
     this.crossDomainMessageMonitorSQL = false
-
-    this.databaseConnected = false
-    this.databaseConnectedMutex = new Mutex()
 
     this.StateCommitmentChainContract = null
     this.L1LiquidityPoolContract = null
@@ -173,11 +138,22 @@ class OptimismEnv {
 
     this.layerZeroEnableTest = LAYER_ZERO_ENABLE_TEST
     this.layerZeroChain = LAYER_ZERO_CHAIN
-    this.layerZeroBridges = LAYER_ZERO_BRIDGES.split(',')
+    this.layerZeroBridges = removeBlankStringInArray(LAYER_ZERO_BRIDGES.split(','))
     this.layerZeroLatestBlock = LAYER_ZERO_LATEST_BLOCK
+
+    this.periodicTransactionPK = PERIODIC_TRANSACTION_PRIVATE_KEY
+    this.periodicTransactionInterval = PERIODIC_TRANSACTION_INTERVAL
+
+    this.bobaStrawContractAddresses = removeBlankStringInArray(BOBASTRAW_CONTACT_ADDRESSES.split(','))
+    this.bobaStrawMonitorInterval = BOBASTRAW_MONITOR_INTERVAL
+
+    this.l1BalanceMonitorAddresses = removeBlankStringInArray(L1_BALANCE_MONITOR_ADDRESSES.split(','))
+    this.l2BalanceMonitorAddresses = removeBlankStringInArray(L2_BALANCE_MONITOR_ADDRESSES.split(','))
+    this.balanceMonitorInterval = BALANCE_MONITOR_INTERVAL
+    /* eslint-enable */
   }
 
-  async initOptimismEnv() {
+  async initGlobalEnv() {
     const addressManager = new ethers.Contract(
       this.addressManagerAddress,
       addressManagerJSON.abi,
@@ -278,4 +254,4 @@ class OptimismEnv {
   }
 }
 
-module.exports = OptimismEnv
+module.exports = GlobalEnv
