50a6e2123ee36abdd85666864cda8c90230ee741
souradeep-das <dsouradeep2@gmail.com>
2023-02-08 17:46:21 +0530

    add alt fee token payment test

--- integration-tests/test/boba_aa_fee_alt_token.spec.ts
+++ integration-tests/test/boba_aa_fee_alt_token.spec.ts
@@ -16,13 +16,14 @@ import MockFeedRegistryJson from '@boba/accountabstraction/artifacts/contracts/t
 import FeedRegistryJson from '@boba/contracts/artifacts/contracts/oracle/FeedRegistry.sol/FeedRegistry.json'
 import SimpleWalletJson from '@boba/accountabstraction/artifacts/contracts/samples/SimpleWallet.sol/SimpleWallet.json'
 import L2GovernanceERC20Json from '@boba/contracts/artifacts/contracts/standards/L2GovernanceERC20.sol/L2GovernanceERC20.json'
+import L2StandardERC20Json from '@eth-optimism/contracts/artifacts/contracts/standards/L2StandardERC20.sol/L2StandardERC20.json'
 import EntryPointJson from '@boba/accountabstraction/artifacts/contracts/core/EntryPoint.sol/EntryPoint.json'
 import { SampleRecipient, SampleRecipient__factory } from '@account-abstraction/utils/dist/src/types'
 import { HttpRpcClient } from '@account-abstraction/sdk/dist/src/HttpRpcClient'
 
-import BobaDepositPaymasterJson from '@boba/accountabstraction/artifacts/contracts/samples/BobaDepositPaymaster.sol/BobaDepositPaymaster.json'
+import ManualDepositPaymasterJson from '@boba/accountabstraction/artifacts/contracts/samples/ManualDepositPaymaster.sol/ManualDepositPaymaster.json'
 
-describe('AA Boba as Fee token Test\n', async () => {
+describe('AA Alt Fee Token Test\n', async () => {
   let env: OptimismEnv
   let SimpleWallet__factory: ContractFactory
   let recipient: Contract
@@ -30,10 +31,11 @@ describe('AA Boba as Fee token Test\n', async () => {
   let bundlerProvider: HttpRpcClient
   let entryPointAddress: string
 
-  let L2BOBAToken: Contract
+  let L2ERC20Token__factory: ContractFactory
+  let L2ERC20Token: Contract
 
-  let BobaDepositPaymaster__factory: ContractFactory
-  let BobaDepositPaymaster: Contract
+  let ManualDepositPaymaster__factory: ContractFactory
+  let ManualDepositPaymaster: Contract
 
   let EntryPoint
 
@@ -43,11 +45,17 @@ describe('AA Boba as Fee token Test\n', async () => {
 
     recipient = await new SampleRecipient__factory(env.l2Wallet).deploy()
 
-    L2BOBAToken = new Contract(
-        env.addressesBOBA.TOKENS.BOBA.L2,
-        L2GovernanceERC20Json.abi,
+    L2ERC20Token__factory = new ContractFactory(
+        L2StandardERC20Json.abi,
+        L2StandardERC20Json.bytecode,
         env.l2Wallet
     )
+
+    // set bridge as wallet_2 to easily mint
+    L2ERC20Token = await L2ERC20Token__factory.deploy(env.l2Wallet_2.address, env.l2Wallet_2.address, 'PEARL', 'PEARL', 18)
+    // mint tokens to wallet
+    console.log(L2ERC20Token.address)
+    await L2ERC20Token.connect(env.l2Wallet_2).mint(env.l2Wallet.address, utils.parseEther('500'))
 
     bundlerProvider = new HttpRpcClient(
       'http://localhost:3000/rpc',
@@ -56,9 +64,9 @@ describe('AA Boba as Fee token Test\n', async () => {
     )
   })
   it('{tag:other} should be able to send a userOperation to a wallet through the bundler', async () => {
-    BobaDepositPaymaster__factory = new ContractFactory(
-        BobaDepositPaymasterJson.abi,
-        BobaDepositPaymasterJson.bytecode,
+    ManualDepositPaymaster__factory = new ContractFactory(
+        ManualDepositPaymasterJson.abi,
+        ManualDepositPaymasterJson.bytecode,
         env.l2Wallet
       )
 
@@ -68,21 +76,24 @@ describe('AA Boba as Fee token Test\n', async () => {
         env.l2Wallet
       )
 
-      const oracleContract = await OracleContract__factory.deploy()
-
-      BobaDepositPaymaster = await BobaDepositPaymaster__factory.deploy(
+      ManualDepositPaymaster = await ManualDepositPaymaster__factory.deploy(
         entryPointAddress,
-        // ethPrice oracle
-        oracleContract.address
       )
 
-    // add boba token
-    await BobaDepositPaymaster.addToken(
-        L2BOBAToken.address,
-        // tokenPrice oracle
-        oracleContract.address,
-        L2BOBAToken.address,
-        18
+    const priceRatio = 100
+    const priceRatioDecimals = 2
+    const minRatio = 1
+    const maxRatio = 500
+
+    // add alt erc20 token
+    await ManualDepositPaymaster.addToken(
+        L2ERC20Token.address,
+        // token decimals
+        await L2ERC20Token.decimals(),
+        priceRatio,
+        priceRatioDecimals,
+        minRatio,
+        maxRatio
     )
 
     EntryPoint = new Contract(
@@ -91,8 +102,8 @@ describe('AA Boba as Fee token Test\n', async () => {
       env.l2Wallet
     )
 
-    await BobaDepositPaymaster.addStake(1, { value: utils.parseEther('2') })
-    await EntryPoint.depositTo(BobaDepositPaymaster.address, {
+    await ManualDepositPaymaster.addStake(1, { value: utils.parseEther('2') })
+    await EntryPoint.depositTo(ManualDepositPaymaster.address, {
       value: utils.parseEther('1')
     })
     // deploy a 4337 Wallet and send operation to this wallet
@@ -107,10 +118,10 @@ describe('AA Boba as Fee token Test\n', async () => {
     )
     await account.deployed()
 
-    await L2BOBAToken.transfer(account.address, utils.parseEther('1'))
+    await L2ERC20Token.transfer(account.address, utils.parseEther('1'))
 
-    await L2BOBAToken.approve(BobaDepositPaymaster.address, constants.MaxUint256)
-    await BobaDepositPaymaster.addDepositFor(L2BOBAToken.address, account.address, utils.parseEther('2'))
+    await L2ERC20Token.approve(ManualDepositPaymaster.address, constants.MaxUint256)
+    await ManualDepositPaymaster.addDepositFor(L2ERC20Token.address, account.address, utils.parseEther('2'))
 
     await env.l2Wallet.sendTransaction({
       value: utils.parseEther('2'),
@@ -125,14 +136,14 @@ describe('AA Boba as Fee token Test\n', async () => {
     })
 
     const approveOp = await walletAPI.createSignedUserOp({
-        target: L2BOBAToken.address,
-        data: L2BOBAToken.interface.encodeFunctionData('approve', [BobaDepositPaymaster.address, constants.MaxUint256]),
+        target: L2ERC20Token.address,
+        data: L2ERC20Token.interface.encodeFunctionData('approve', [ManualDepositPaymaster.address, constants.MaxUint256]),
         maxFeePerGas: '0x59682F00',
         maxPriorityFeePerGas: '0x59682F00',
     })
 
-    const preApproveTokenBalance = await L2BOBAToken.balanceOf(account.address)
-    const preApproveDepositAmount = (await BobaDepositPaymaster.depositInfo(L2BOBAToken.address, account.address)).amount
+    const preApproveTokenBalance = await L2ERC20Token.balanceOf(account.address)
+    const preApproveDepositAmount = (await ManualDepositPaymaster.depositInfo(L2ERC20Token.address, account.address)).amount
     const preApproveEtherBalance = await env.l2Provider.getBalance(account.address)
 
     try {
@@ -144,8 +155,8 @@ describe('AA Boba as Fee token Test\n', async () => {
         throw new Error('Submission to Bundler Failed')
     }
 
-    const postApproveTokenBalance = await L2BOBAToken.balanceOf(account.address)
-    const postApproveDepositAmount = (await BobaDepositPaymaster.depositInfo(L2BOBAToken.address, account.address)).amount
+    const postApproveTokenBalance = await L2ERC20Token.balanceOf(account.address)
+    const postApproveDepositAmount = (await ManualDepositPaymaster.depositInfo(L2ERC20Token.address, account.address)).amount
     const postApproveEtherBalance = await env.l2Provider.getBalance(account.address)
     const op = await walletAPI.createUnsignedUserOp({
       target: recipient.address,
@@ -155,7 +166,7 @@ describe('AA Boba as Fee token Test\n', async () => {
     })
 
 
-    op.paymasterAndData = hexConcat([BobaDepositPaymaster.address, hexZeroPad(L2BOBAToken.address, 20)])
+    op.paymasterAndData = hexConcat([ManualDepositPaymaster.address, hexZeroPad(L2ERC20Token.address, 20)])
     op.preVerificationGas = await walletAPI.getPreVerificationGas(op)
 
     const signedOp = await walletAPI.signUserOp(op)
@@ -178,8 +189,8 @@ describe('AA Boba as Fee token Test\n', async () => {
       expect(log.args.msgSender).to.eq(account.address)
       // message is received and emitted
       expect(log.args.message).to.eq('hello')
-      const postCallTokenBalance = await L2BOBAToken.balanceOf(account.address)
-      const postCallDepositAmount = (await BobaDepositPaymaster.depositInfo(L2BOBAToken.address, account.address)).amount
+      const postCallTokenBalance = await L2ERC20Token.balanceOf(account.address)
+      const postCallDepositAmount = (await ManualDepositPaymaster.depositInfo(L2ERC20Token.address, account.address)).amount
       const postCallEtherBalance = await env.l2Provider.getBalance(account.address)
 
       const returnedEPlogIndex = await getFilteredLogIndex(
@@ -199,7 +210,6 @@ describe('AA Boba as Fee token Test\n', async () => {
       // no ether is used when calling the recipient with the help of the paymaster, users boba token is used to pay
       expect(postApproveEtherBalance).to.eq(postCallEtherBalance)
       expect(postApproveTokenBalance).to.gt(postCallTokenBalance)
-      expect(BigNumber.from(postCallTokenBalance).add(logEP.args.actualGasCost)).to.closeTo(BigNumber.from(postApproveTokenBalance), utils.parseEther('0.0001'))
     } catch (e) {
       console.log(e)
       throw new Error('Submission to Bundler Failed')
