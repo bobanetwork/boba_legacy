fac912c076f67fa806bea703511a42fe0de5a943
souradeep-das <dsouradeep2@gmail.com>
2023-02-14 22:43:21 +0530

    add deterministicDeployer test

--- packages/boba/bundler/packages/sdk/test/0-deterministicDeployer.test.ts
+++ packages/boba/bundler/packages/sdk/test/0-deterministicDeployer.test.ts
@@ -23,4 +23,23 @@ describe('#deterministicDeployer', () => {
     await DeterministicDeployer.deploy(ctr)
     expect(await deployer.isContractDeployed(addr)).to.equal(true)
   })
+  it('should deploy at given address with ethers.wallet', async () => {
+    const randWallet = ethers.Wallet.createRandom();
+    const pk = (randWallet._signingKey()).privateKey
+    // generate a ethers.wallet to use with sdk
+    const testWallet = new ethers.Wallet(pk, ethers.provider)
+    // send funds to this wallet
+    await ethers.provider.getSigner().sendTransaction({to: testWallet.address, value: ethers.utils.parseEther('0.1')})
+
+    const ctr = hexValue(new SampleRecipient__factory(ethers.provider.getSigner()).getDeployTransaction().data!)
+    DeterministicDeployer.init(ethers.provider, testWallet)
+    const addr = await DeterministicDeployer.getAddress(ctr, 1)
+    expect(await deployer.isContractDeployed(addr)).to.equal(false)
+    const preWalletBalance = await ethers.provider.getBalance(testWallet.address)
+    await DeterministicDeployer.deploy(ctr, 1)
+    const postWalletBalance = await ethers.provider.getBalance(testWallet.address)
+    expect(await deployer.isContractDeployed(addr)).to.equal(true)
+    // wallet deploys contract
+    expect(postWalletBalance).to.lt(preWalletBalance)
+  })
 })
