42c2a5993d21129b6f2a8c5941b2f3d70b424209
Boyuan-Chen <46272347+Boyuan-Chen@users.noreply.github.com>
2023-02-18 13:06:04 -0500

    Fix boba link api and tests (#596)

--- integration-tests/test/bobalink.spec.ts
+++ integration-tests/test/bobalink.spec.ts
@@ -191,7 +191,7 @@ describe('BobaLink Test\n', async () => {
             const response = await asyncBobaLinkGetQuote({
               body: JSON.stringify({params: [input]}
             )}, null)
-            res.end(JSON.stringify(response))
+            res.end(response.body)
             server.emit('success', body)
           }
           if (req.url === '/invalidapi') {
@@ -205,9 +205,9 @@ describe('BobaLink Test\n', async () => {
         res.writeHead(400, { 'Content-Type': 'text/plain' })
         res.end('Expected content-type: application/json')
       }
-      }).listen(apiPort)
-      URL = `http://${ip.address()}:${apiPort}`
-      /* eslint-enable */
+    }).listen(apiPort)
+    URL = `http://${ip.address()}:${apiPort}`
+    /* eslint-enable */
   })
 
   it('test of local compute endpoint: should return price', async () => {
@@ -287,7 +287,7 @@ describe('BobaLink Test\n', async () => {
     })
     const res = await resp.json()
     const result = utils.defaultAbiCoder.decode(
-      ['uint256', 'uint256', 'int256', 'uint80'],
+      ['uint256', 'uint256', 'int256', 'uint256'],
       res.result
     )
     expect(Number(result[0])).to.equal(32 * 3)
@@ -308,7 +308,7 @@ describe('BobaLink Test\n', async () => {
     )
     expect(chainLinkQuoteEvents[0].args.CLRoundId).to.equal(lastRoundId)
     expect(chainLinkQuoteEvents[0].args.CLLatestRoundId).to.eq(lastRoundId)
-  }).retries(3)
+  })
 
   it('should get a single quote via bobalink using test api', async () => {
     await EthOracleHC.updateHCUrl(`${URL}/bobalink-test-api`)
