5f943973f446a786942ba8ca782710c789bda1ae
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2023-03-01 14:03:04 +0100

    extract bundler sub-packages

--- packages/boba/bundler/packages/bundler/contracts/Import.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// import contracts to get their type info.
-import "@account-abstraction/utils/contracts/test/SampleRecipient.sol";
-import "@account-abstraction/utils/contracts/test/SingletonFactory.sol";
-import "@account-abstraction/contracts/samples/SimpleWalletDeployer.sol";
