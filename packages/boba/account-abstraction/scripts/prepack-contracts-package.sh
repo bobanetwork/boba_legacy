9240bae2c29086189310e39726745ea1fbb02fc5
souradeep-das <dsouradeep2@gmail.com>
2023-03-02 16:17:13 +0530

    update prepack script

--- packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
+++ packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
@@ -4,11 +4,8 @@
 cd `dirname $0`/..
 pwd
 
-yarn clean
-yarn compile
-
 mkdir -p artifacts_clean
 cp `find  ./artifacts/contracts -type f | grep -v -E 'Test|dbg|gnosis|bls|IOracle'` artifacts_clean/
-typechain --target ethers-v5 --out-dir types artifacts_clean/**
+npx typechain --target ethers-v5 --out-dir types artifacts_clean/**
 rm -r artifacts_clean
 npx tsc index.ts -d --outDir dist
