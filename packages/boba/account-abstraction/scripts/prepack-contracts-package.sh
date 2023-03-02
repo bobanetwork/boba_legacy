4bd7ef34042595919123460e4583e7365e37a232
souradeep-das <dsouradeep2@gmail.com>
2023-03-02 13:56:09 +0530

    check version

--- packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
+++ packages/boba/account-abstraction/scripts/prepack-contracts-package.sh
@@ -9,6 +9,6 @@ yarn compile
 
 mkdir -p artifacts_clean
 cp `find  ./artifacts/contracts -type f | grep -v -E 'Test|dbg|gnosis|bls|IOracle'` artifacts_clean/
-npx typechain --target ethers-v5 --out-dir types artifacts_clean/**
+typechain --target ethers-v5 --out-dir types artifacts_clean/**
 rm -r artifacts_clean
-npx tsc index.ts -d --outDir dist
+tsc index.ts -d --outDir dist
