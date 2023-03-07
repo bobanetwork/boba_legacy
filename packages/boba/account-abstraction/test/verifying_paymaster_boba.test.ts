404003b2f59bd7e6a6258120a9430ec1c2d35aad
souradeep-das <dsouradeep2@gmail.com>
2023-03-07 16:08:02 +0530

    bump aa contracts with 0.3.0 changes

--- packages/boba/account-abstraction/test/verifying_paymaster_boba.test.ts
+++ packages/boba/account-abstraction/test/verifying_paymaster_boba.test.ts
@@ -2,8 +2,8 @@ import { Wallet } from 'ethers'
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import {
-  SimpleWallet,
-  SimpleWallet__factory,
+  SimpleAccount,
+  SimpleAccount__factory,
   EntryPoint,
   BobaVerifyingPaymaster,
   BobaVerifyingPaymaster__factory,
@@ -15,7 +15,7 @@ import {
   TestToken__factory
 } from '../typechain'
 import {
-  createWalletOwner,
+  createAccountOwner,
   deployEntryPoint, simulationResultCatch
 } from './testutils'
 import { fillAndSign } from './UserOp'
@@ -23,9 +23,9 @@ import { arrayify, hexConcat, parseEther } from 'ethers/lib/utils'
 
 describe('EntryPoint with VerifyingPaymaster', function () {
   let entryPoint: EntryPoint
-  let walletOwner: Wallet
+  let accountOwner: Wallet
   const ethersSigner = ethers.provider.getSigner()
-  let wallet: SimpleWallet
+  let account: SimpleAccount
   let offchainSigner: Wallet
 
   let paymaster: BobaVerifyingPaymaster
@@ -35,8 +35,8 @@ describe('EntryPoint with VerifyingPaymaster', function () {
   before(async function () {
     entryPoint = await deployEntryPoint()
 
-    offchainSigner = createWalletOwner()
-    walletOwner = createWalletOwner()
+    offchainSigner = createAccountOwner()
+    accountOwner = createAccountOwner()
 
     ethOracle = await new MockFeedRegistry__factory(ethersSigner).deploy()
     token = await new TestToken__factory(ethersSigner).deploy()
@@ -47,71 +47,71 @@ describe('EntryPoint with VerifyingPaymaster', function () {
     paymaster = await new BobaVerifyingPaymaster__factory(ethersSigner).deploy(entryPoint.address, offchainSigner.address, depositPaymaster.address, token.address)
     await paymaster.addStake(1, { value: parseEther('2') })
     await entryPoint.depositTo(paymaster.address, { value: parseEther('1') })
-    wallet = await new SimpleWallet__factory(ethersSigner).deploy(entryPoint.address, walletOwner.address)
+    account = await new SimpleAccount__factory(ethersSigner).deploy(entryPoint.address, accountOwner.address)
   })
 
   describe('#validatePaymasterUserOp', () => {
     it('should reject on no signature', async () => {
       const userOp = await fillAndSign({
-        sender: wallet.address,
+        sender: account.address,
         paymasterAndData: hexConcat([paymaster.address, '0x1234'])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await expect(entryPoint.callStatic.simulateValidation(userOp)).to.be.revertedWith('invalid signature length in paymasterAndData')
     })
 
     it('should reject on invalid signature', async () => {
       const userOp = await fillAndSign({
-        sender: wallet.address,
+        sender: account.address,
         paymasterAndData: hexConcat([paymaster.address, '0x' + '1c'.repeat(65)])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await expect(entryPoint.callStatic.simulateValidation(userOp)).to.be.revertedWith('ECDSA: invalid signature')
     })
 
     it('should reject on unsupported calldata', async () => {
-      const approveData = token.interface.encodeFunctionData('mint', [wallet.address, 1000])
+      const approveData = token.interface.encodeFunctionData('mint', [account.address, 1000])
       const userOp1 = await fillAndSign({
-        sender: wallet.address,
-        callData: wallet.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
-      }, walletOwner, entryPoint)
+        sender: account.address,
+        callData: account.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
+      }, accountOwner, entryPoint)
       const hash = await paymaster.getHash(userOp1)
       const sig = await offchainSigner.signMessage(arrayify(hash))
 
       const userOp = await fillAndSign({
         ...userOp1,
         paymasterAndData: hexConcat([paymaster.address, sig])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await expect(entryPoint.callStatic.simulateValidation(userOp)).to.be.revertedWith('VerifyingPaymaster: invalid operation')
     })
 
     it('should reject on unsupported paymasters', async () => {
       const approveData = token.interface.encodeFunctionData('approve', [paymaster.address, ethers.constants.MaxUint256])
       const userOp1 = await fillAndSign({
-        sender: wallet.address,
-        callData: wallet.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
-      }, walletOwner, entryPoint)
+        sender: account.address,
+        callData: account.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
+      }, accountOwner, entryPoint)
       const hash = await paymaster.getHash(userOp1)
       const sig = await offchainSigner.signMessage(arrayify(hash))
 
       const userOp = await fillAndSign({
         ...userOp1,
         paymasterAndData: hexConcat([paymaster.address, sig])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await expect(entryPoint.callStatic.simulateValidation(userOp)).to.be.revertedWith('VerifyingPaymaster: invalid operation')
     })
 
     it('succeed with valid signature and approve calldata', async () => {
       const approveData = token.interface.encodeFunctionData('approve', [depositPaymaster.address, ethers.constants.MaxUint256])
       const userOp1 = await fillAndSign({
-        sender: wallet.address,
-        callData: wallet.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
-      }, walletOwner, entryPoint)
+        sender: account.address,
+        callData: account.interface.encodeFunctionData('execFromEntryPoint', [token.address, 0, approveData])
+      }, accountOwner, entryPoint)
       const hash = await paymaster.getHash(userOp1)
       const sig = await offchainSigner.signMessage(arrayify(hash))
 
       const userOp = await fillAndSign({
         ...userOp1,
         paymasterAndData: hexConcat([paymaster.address, sig])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await entryPoint.callStatic.simulateValidation(userOp).catch(simulationResultCatch)
     })
 
@@ -119,34 +119,34 @@ describe('EntryPoint with VerifyingPaymaster', function () {
       // amount zero for skipping approval
       const tokenAlt = await new TestToken__factory(ethersSigner).deploy()
       await depositPaymaster.addToken(tokenAlt.address, ethOracle.address, tokenAlt.address, 18)
-      const depositData = depositPaymaster.interface.encodeFunctionData('addDepositFor', [tokenAlt.address, wallet.address, 0])
+      const depositData = depositPaymaster.interface.encodeFunctionData('addDepositFor', [tokenAlt.address, account.address, 0])
       const userOp1 = await fillAndSign({
-        sender: wallet.address,
+        sender: account.address,
         // incorrect calldata
-        callData: wallet.interface.encodeFunctionData('execFromEntryPoint', [depositPaymaster.address, 0, depositData])
-      }, walletOwner, entryPoint)
+        callData: account.interface.encodeFunctionData('execFromEntryPoint', [depositPaymaster.address, 0, depositData])
+      }, accountOwner, entryPoint)
       const hash = await paymaster.getHash(userOp1)
       const sig = await offchainSigner.signMessage(arrayify(hash))
       const userOp = await fillAndSign({
         ...userOp1,
         paymasterAndData: hexConcat([paymaster.address, sig])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await expect(entryPoint.callStatic.simulateValidation(userOp)).to.be.revertedWith('VerifyingPaymaster: invalid operation')
     })
 
     it('succeed with valid signature and deposit calldata', async () => {
       // amount zero for skipping approval
-      const depositData = depositPaymaster.interface.encodeFunctionData('addDepositFor', [token.address, wallet.address, 0])
+      const depositData = depositPaymaster.interface.encodeFunctionData('addDepositFor', [token.address, account.address, 0])
       const userOp1 = await fillAndSign({
-        sender: wallet.address,
-        callData: wallet.interface.encodeFunctionData('execFromEntryPoint', [depositPaymaster.address, 0, depositData])
-      }, walletOwner, entryPoint)
+        sender: account.address,
+        callData: account.interface.encodeFunctionData('execFromEntryPoint', [depositPaymaster.address, 0, depositData])
+      }, accountOwner, entryPoint)
       const hash = await paymaster.getHash(userOp1)
       const sig = await offchainSigner.signMessage(arrayify(hash))
       const userOp = await fillAndSign({
         ...userOp1,
         paymasterAndData: hexConcat([paymaster.address, sig])
-      }, walletOwner, entryPoint)
+      }, accountOwner, entryPoint)
       await entryPoint.callStatic.simulateValidation(userOp).catch(simulationResultCatch)
     })
   })
