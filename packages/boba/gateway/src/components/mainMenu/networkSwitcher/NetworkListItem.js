2103b0a8f8b804540fddfa9e3e6318cf98edd1a9
Sahil Kashetwar <sahil@enya.ai>
2023-04-04 10:39:21 +0530

    fix: network icons

--- packages/boba/gateway/src/components/mainMenu/networkSwitcher/NetworkListItem.js
+++ packages/boba/gateway/src/components/mainMenu/networkSwitcher/NetworkListItem.js
@@ -15,6 +15,15 @@ function NetworkListItem({
   isActive
 }) {
 
+  console.table({
+    chain,
+    icon,
+    label,
+    name,
+    onChainChange,
+    isActive
+  })
+
   const L1Icon = L1_ICONS[ icon ];
   const L2Icon = L2_ICONS[ icon ];
 
