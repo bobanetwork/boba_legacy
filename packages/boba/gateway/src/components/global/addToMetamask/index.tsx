55541db9de8d78f3c52df5864533062a723ff781
alvaro-ricotta <81116391+alvaro-ricotta@users.noreply.github.com>
2023-04-14 20:35:37 -0300

    update components + views (#712)
    (cherry picked from commit 6614b3f2b42504aadfe5459dce83a2be2675f8be)

---
+++ packages/boba/gateway/src/components/global/addToMetamask/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { getCoinImage } from 'util/coinImage'
+import MetamaskLogo from 'images/metamask.svg'
+import networkService from 'services/networkService'
+import styled from 'styled-components'
+
+type TokenType = {
+  symbol: string
+  decimals: number
+  address: string
+  chain: string
+}
+
+type AddToMetamaskType = {
+  token: TokenType
+  className: string
+}
+
+const AddToMetamaskContainer = styled.div`
+  margin-left: auto;
+  cursor: pointer;
+`
+
+export const AddToMetamask = ({
+  token,
+  className,
+}: AddToMetamaskType): JSX.Element => {
+  const { symbol } = token || {}
+  const logoURI = getCoinImage(symbol)
+  const handleAddToMetamask = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation()
+    networkService.walletService.addTokenToMetaMask({ ...token, logoURI })
+  }
+
+  return (
+    <AddToMetamaskContainer
+      onClick={(e) => handleAddToMetamask(e)}
+      className={className}
+    >
+      <img
+        src={MetamaskLogo}
+        alt="add To Metamask"
+        width="20px"
+        height="20px"
+      />
+    </AddToMetamaskContainer>
+  )
+}
