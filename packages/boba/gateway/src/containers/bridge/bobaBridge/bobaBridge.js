73d32eb4485138393c518348596b72556a8839f3
Sahil Kashetwar <sahil@enya.ai>
2023-04-19 20:44:25 +0530

    added check on account enabled instead of walletAddress for connect btn

--- packages/boba/gateway/src/containers/bridge/bobaBridge/bobaBridge.js
+++ packages/boba/gateway/src/containers/bridge/bobaBridge/bobaBridge.js
@@ -166,7 +166,7 @@ function BobaBridge() {
           </Box>
         </S.BobaContentWrapper>
       </S.BobaContent>
-      {!userWallet &&
+      {!accountEnabled &&
         <Box alignSelf="center">
           <Button
             fullWidth={true}
