a27192c38ba59a4d69bf361880b15ecc4767a598
Sahil Kashetwar <sahil@enya.ai>
2023-04-25 03:55:29 +0530

    change:  - env driven banner for bobabeam to withdraw fund  - enable banner only for moonbeam network

--- packages/boba/gateway/src/containers/home/Home.js
+++ packages/boba/gateway/src/containers/home/Home.js
@@ -88,7 +88,7 @@ import Alert from 'components/alert/Alert'
 import LayerSwitcher from 'components/mainMenu/layerSwitcher/LayerSwitcher'
 
 /******** UTILS ********/
-import { APP_STATUS, POLL_INTERVAL } from 'util/constant'
+import { APP_STATUS, BOBABEAM_STATUS, POLL_INTERVAL } from 'util/constant'
 import useInterval from 'hooks/useInterval'
 import useGoogleAnalytics from 'hooks/useGoogleAnalytics'
 import useNetwork from 'hooks/useNetwork'
@@ -257,10 +257,13 @@ function Home() {
       {!!CDMCompletionModalState && <CDMCompletionModal open={CDMCompletionModalState} />}
       {!!switchNetworkModalState && <SwitchNetworkModal open={switchNetworkModalState} />}
 
-      <NotificationBar
-        message='Bobabeam is closing - please withdraw your funds'
-        open={true}
-      />
+      {
+        !!Number(BOBABEAM_STATUS)
+        && activeNetwork === 'MOONBEAM'
+        && <NotificationBar
+          message='Bobabeam is closing - please withdraw your funds'
+          open={true}
+        />}
 
       <Alert
         type='error'
