bda763bef3f86d9fe7382508e935320af6a07b40
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2023-04-24 14:59:43 -0300

    remove unnused component

--- packages/boba/gateway/src/components/index.ts
+++ packages/boba/gateway/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Copy } from 'components/copy/Copy'
-export { default as Disconnect } from 'components/copy/Copy'
+export { default as Disconnect } from 'components/disconnect/Disconnect'
 export { default as CounterButton } from 'components/counterButton/CounterButton'
+
 export * from 'components/mainMenu/'
