bb2776818ea0f2a6ee2897f201ec7748f7e4f20e
Sahil Kashetwar <sahil@enya.ai>
2023-04-25 03:33:21 +0530

    Add themes with support for theme switcher

---
+++ packages/boba/gateway/src/themes/dark.ts
@@ -0,0 +1,14 @@
+const dark = {
+  name: 'dark',
+  primarybg: '#BAE21A', //cyan
+  primaryfg: '#000000', //cyan
+  colors: {
+    primary: '#BAE21A', // radioGreen
+    background: '#111315',
+    text: '#FFFFFF',
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+}
+
+export default dark
---
+++ packages/boba/gateway/src/themes/light.ts
@@ -0,0 +1,14 @@
+const light = {
+  name: 'light',
+  primarybg: '#1CD6D1', //cyan
+  primaryfg: '#000000',
+  colors: {
+    primary: '#1CD6D1', //cyan
+    background: '#FFFFFF',
+    text: '#000000',
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+}
+
+export default light
