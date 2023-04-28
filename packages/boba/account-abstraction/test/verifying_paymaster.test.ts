507c8cd728fb6e47bb42b9f742a4c6e06e7a9ba1
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2023-04-28 13:21:18 +0200

    sponsoring fee fixed

--- packages/boba/account-abstraction/test/verifying_paymaster.test.ts
+++ packages/boba/account-abstraction/test/verifying_paymaster.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+/* eslint-disable prettier/prettier */
 import { Wallet } from 'ethers'
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
