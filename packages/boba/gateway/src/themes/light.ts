dcf9b7e3b992f88cc3c1b42250fd6ad3db960c31
Sahil K <86316370+sk-enya@users.noreply.github.com>
2023-05-07 12:58:33 +0530

    Fix/banxa and bridges (#772)
    * adding boba network
    
    * fixing boba bridge url
    
    * fixing bridge integration
    
    * replace code by selectors
    
    * remove hardcoded symbol
    
    * enable banxa only for mainnet
    
    * Available bridge inable only for mainnet
    
    * adding support for testnet
    
    * update conditional for other bridges
    
    * implemented the available bridges with typescript
    
    * unit test cases for available bridges
    
    * typo in Available bridges
    
    ---------
    
    Co-authored-by: alvaro-ricotta <alvaro.e.ricotta@gmail.com>
    Co-authored-by: alvaro-ricotta <81116391+alvaro-ricotta@users.noreply.github.com>
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>

--- packages/boba/gateway/src/themes/light.ts
+++ packages/boba/gateway/src/themes/light.ts
@@ -6,6 +6,11 @@ const light = {
   primaryfg: '#000000',
   screen,
   warning: 'yellow',
+  border: 'solid 1px rgba(0, 0, 0, 0.12)',
+  bg: {
+    glassy: 'rgba(0,0,0, 0.09)',
+    secondary: 'rgba(0, 0, 0, 0.08)',
+  },
 }
 
 export default light
