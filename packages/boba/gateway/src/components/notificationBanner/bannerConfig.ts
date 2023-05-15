11b26233c52d6f8c5214c4da419257de6d3eef53
Sahil Kashetwar <sahil@enya.ai>
2023-05-16 00:45:10 +0530

    updated test cases for notification banner

--- packages/boba/gateway/src/components/notificationBanner/bannerConfig.ts
+++ packages/boba/gateway/src/components/notificationBanner/bannerConfig.ts
@@ -12,7 +12,7 @@ export const BannerConfig: Record<string, BannerContent> = {
     For users of Bobabeam or Bobabeam applications you will need to transfer all your funds to Moonbeam mainnet before May 15th or risk permanently losing access to any assets on Bobabeam.`,
   },
   [NETWORK.FANTOM]: {
-    message: `BobaOpera is being wound down & will no longer be available soon, `,
+    message: `BobaOpera is being wound down & will no longer be available soon`,
     content: `BobaOpera is being wound down & will no longer be available soon, For users of BobaOpera or BobaOpera applications you will need to transfer all your funds to Fantom mainnet as earliest possible or risk permanently losing access to any assets on BobaOpera.`,
   },
 }
