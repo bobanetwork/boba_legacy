d4b718141bb3fda11d6416bc60de02d5115684f6
Sahil K <86316370+sk-enya@users.noreply.github.com>
2023-05-24 21:16:11 +0530

    Fix : Alert for Boba Opera shutting down (#814)
    * fantom opera disable alert + textual change for moonbeam alert
    
    * updated test cases for notification banner
    
    * updated dates for the opera shutdown
    
    * updated test cases
    
    * updated test cases failure
    
    (cherry picked from commit f949b705c0de1e1799ac83125e4fcd938c802822)

--- packages/boba/gateway/src/containers/modals/deposit/steps/InputStepFast.js
+++ packages/boba/gateway/src/containers/modals/deposit/steps/InputStepFast.js
@@ -63,7 +63,7 @@ import {
   fetchL2LPLiquidity,
 } from 'actions/balanceAction'
 
-import BobaBeamAlert from './bobaBeamAlert'
+import BridgeAlert from './BridgeAlert'
 
 
 function InputStepFast({ handleClose, token, isBridge, openTokenPicker }) {
@@ -295,7 +295,7 @@ function InputStepFast({ handleClose, token, isBridge, openTokenPicker }) {
     //no token in this account
     return (
       <Box>
-        <BobaBeamAlert />
+        <BridgeAlert />
         <Typography
           variant="body2"
           sx={{ fontWeight: 700, mb: 1, color: 'yellow' }}
@@ -320,7 +320,7 @@ function InputStepFast({ handleClose, token, isBridge, openTokenPicker }) {
   return (
     <>
       <Box>
-        <BobaBeamAlert />
+        <BridgeAlert />
         {!isBridge && (
           <Typography variant="h2" sx={{ fontWeight: 700, mb: 1 }}>
             Fast Bridge to L2
