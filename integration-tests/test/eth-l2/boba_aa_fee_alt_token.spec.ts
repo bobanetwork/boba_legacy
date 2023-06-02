b12808e59e84eaf59e04a76a8e4e5aeb98c5ea56
Ino Murko <ino.murko.github@protonmail.com>
2023-06-02 18:16:50 +0200

    Release/v0.7.1 (#876)
    * [AA] First draft defaults (#837)
    
    * first draft defaults
    
    * feedback, defaults
    
    * bundler fix: number<>string defaults
    
    ---------
    
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>
    (cherry picked from commit 41a6affb3f9defd4d7ac8ff13db57f17831d7f3f)
    
    * [AA] optimization: paymasterAndData for aa-sdk (#871)
    
    * paymaster api
    
    * switch-to-signed
    
    * generalized, tests adapted
    
    (cherry picked from commit 710270e28c85a3a9fe3fea97ca4e4377659a9130)
    
    * fix for crash on wallet page (#873)
    
    (cherry picked from commit e93822da5cdd5a6ab19292237eb84cab4e666a3a)
    
    * remove unused vars (#875)
    
    (cherry picked from commit fbb0a89f9b8abf2d4ab04eceb196457923833691)
    
    ---------
    
    Co-authored-by: Riedl Kevin, Bsc <kevin.riedl@wavect.io>
    Co-authored-by: Sahil K <86316370+sk-enya@users.noreply.github.com>

--- integration-tests/test/eth-l2/boba_aa_fee_alt_token.spec.ts
+++ integration-tests/test/eth-l2/boba_aa_fee_alt_token.spec.ts
@@ -10,7 +10,7 @@ import { getFilteredLogIndex } from './shared/utils'
 import { OptimismEnv } from './shared/env'
 import { hexConcat, hexZeroPad, parseEther } from 'ethers/lib/utils'
 // use local sdk
-import { SimpleAccountAPI } from '@bobanetwork/bundler_sdk'
+import { PaymasterAPI, SimpleAccountAPI } from "@bobanetwork/bundler_sdk";
 import SenderCreatorJson from '@boba/accountabstraction/artifacts/contracts/core/SenderCreator.sol/SenderCreator.json'
 import SimpleAccountFactoryJson from '@boba/accountabstraction/artifacts/contracts/samples/SimpleAccountFactory.sol/SimpleAccountFactory.json'
 import L2StandardERC20Json from '@eth-optimism/contracts/artifacts/contracts/standards/L2StandardERC20.sol/L2StandardERC20.json'
@@ -210,17 +210,16 @@ describe('AA Alt Fee Token Test\n', async () => {
     )
 
     it('should be able to submit a userOp including the paymaster to the bundler and trigger tx', async () => {
-      const op = await accountAPI.createUnsignedUserOp({
+      accountAPI.paymasterAPI = new PaymasterAPI({
+        paymasterAndData: hexConcat([
+          ManualDepositPaymaster.address,
+          hexZeroPad(L2ERC20Token.address, 20),
+        ]),
+      })
+      signedOp = await accountAPI.createSignedUserOp({
         target: recipient.address,
         data: recipient.interface.encodeFunctionData('something', ['hello']),
       })
-      op.paymasterAndData = hexConcat([
-        ManualDepositPaymaster.address,
-        hexZeroPad(L2ERC20Token.address, 20),
-      ])
-      op.preVerificationGas = await accountAPI.getPreVerificationGas(op)
-
-      signedOp = await accountAPI.signUserOp(op)
 
       const requestId = await bundlerProvider.sendUserOpToBundler(signedOp)
       const txid = await accountAPI.getUserOpReceipt(requestId)
