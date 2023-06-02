a665687bbd774df8616ef3b944f5b8cabfbfecd8
sahil kashetwar <sahilkashetwar24@gmail.com>
2023-06-03 03:40:00 +0530

    change:  - updated .nvmrc version of node to 16.16.0  - added node-gyp as dev deps to fix the yarn install prebuild error  - spin up the local and executed integrations, unit test to valid the version update  - updated the readme with details to build and spin up side services to execute the integration-tests.

--- integration-tests/hardhat.config.ts
+++ integration-tests/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from 'hardhat/types'
 // Hardhat plugins
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import 'hardhat-gas-reporter'
 import 'mocha-multi-reporters'
 
 const config: HardhatUserConfig = {
