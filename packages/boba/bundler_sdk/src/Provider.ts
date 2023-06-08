f2373aeffc414de375eba4b09bbad9613319bc50
Riedl Kevin, Bsc <kevin.riedl@wavect.io>
2023-06-08 17:16:11 +0200

    Add BNB AA example & Add BNB testnet config (#909)
    * bnb integ test
    
    * docs
    
    * docs
    
    * Update aa-bundler-sdk-pr.yml
    
    * Update README.md

--- packages/boba/bundler_sdk/src/Provider.ts
+++ packages/boba/bundler_sdk/src/Provider.ts
@@ -21,17 +21,23 @@ const debug = Debug('aa.wrapProvider')
  * @param originalSigner use this signer as the owner. of this wallet. By default, use the provider's signer
  * @param entryPointWrapperAddress boba does not return revert data for custom errors, if on boba pass an entryPointWrapperAddress to compute account address
  * @param wallet optional, boba does not allow eth_sendTransaction from a remote signer, if on boba pass wallet
+ * @param networkName optional, provide network name for deterministicDeployer config.
  */
 export async function wrapProvider (
   originalProvider: JsonRpcProvider,
   config: ClientConfig,
   originalSigner: Signer = originalProvider.getSigner(),
   entryPointWrapperAddress: string,
-  wallet?: Wallet
+  wallet?: Wallet,
+  networkName?: string
 ): Promise<ERC4337EthersProvider> {
   const entryPoint = EntryPoint__factory.connect(config.entryPointAddress, originalProvider)
   // Initial SimpleAccount instance is not deployed and exists just for the interface
-  const detDeployer = new DeterministicDeployer(originalProvider, wallet)
+  const detDeployer = new DeterministicDeployer(
+    originalProvider,
+    wallet,
+    networkName
+  )
   const SimpleAccountFactory = await detDeployer.deterministicDeploy(new SimpleAccountFactory__factory(), 0, [entryPoint.address])
   let smartWalletAPIOwner
    if (wallet != null) {
