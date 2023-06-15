e81de075733eaaf9032016a93afa16d0b6a0177f
cby3149 <cby3149@gmail.com>
2023-06-15 13:28:56 -0400

    Report result

--- go/proxyd/debug.go
+++ go/proxyd/debug.go
@@ -12,11 +12,11 @@ func debugResult(ctx context.Context, method string, defaultRes *RPCRes, debugRe
 	reqID := GetReqID(ctx)
 	debugResult, ok := debugRes.Result.(string)
 	if !ok {
-		log.Error("Failed to decode debugResult", "method", method, "debugResult", debugResult, "req_id", reqID)
+		log.Error("Failed to decode debugResult", "method", method, "debugResult", debugResult, "req_id", reqID, "result", debugRes.Result)
 	}
 	defaultResult, ok := defaultRes.Result.(string)
 	if !ok {
-		log.Error("Failed to decode defaultResult", "method", method, "defaultResult", defaultResult, "req_id", reqID)
+		log.Error("Failed to decode defaultResult", "method", method, "defaultResult", defaultResult, "req_id", reqID, "result", defaultRes.Result)
 	}
 	if debugResult != defaultResult {
 		log.Error("Found difference", "method", method, "debugResult", debugResult, "defaultResult", defaultResult, "req_id", reqID)
@@ -31,11 +31,11 @@ func debugLogs(ctx context.Context, method string, defaultRes *RPCRes, debugRes 
 	var defaultLogs *types.Log
 	debugLogs, ok := debugRes.Result.(*types.Log)
 	if !ok {
-		log.Error("Failed to decode debugLogs", "method", method, "debugLogs", debugLogs, "req_id", reqID)
+		log.Error("Failed to decode debugLogs", "method", method, "debugLogs", debugLogs, "req_id", reqID, "result", debugRes.Result)
 	}
 	defaultLogs, ok = defaultRes.Result.(*types.Log)
 	if !ok {
-		log.Error("Failed to decode defaultLogs", "method", method, "defaultLogs", defaultLogs, "req_id", reqID)
+		log.Error("Failed to decode defaultLogs", "method", method, "defaultLogs", defaultLogs, "req_id", reqID, "result", defaultRes.Result)
 	}
 	if !reflect.DeepEqual(debugLogs, defaultLogs) {
 		log.Error("Found difference", "method", method, "debugLogs", debugLogs, "defaultLogs", defaultLogs, "req_id", reqID)
