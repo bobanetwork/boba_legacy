29665aba03561250570f9df2acd2285f9c1e3553
Riedl Kevin, Bsc <kevin.riedl@wavect.io>
2023-06-19 22:25:38 +0200

    Switch from file-based db to postgresql with DB migrations (#822)
    * stage pre
    
    * install
    
    * typeorm
    
    * typeorm, tests debugging
    
    * Delete package-lock.json
    
    * progress
    
    * debug
    
    * fixed tests
    
    * logging
    
    * depcheck
    
    * teleporter moved over to integration tests
    
    * Update packages/boba/teleportation/package.json
    
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>
    
    * change teleporter env name
    
    * tests passing
    
    * cleanup
    
    * rm only
    
    * Updated config.yml
    
    * Updated config.yml
    
    * db circleci fix
    
    * Updated config.yml
    
    Updated config.yml
    
    Updated config.yml
    
    Updated config.yml
    
    * uplocal
    
    * Updated config.yml
    
    * Update up_local.sh
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * fix-dockerfilepackages
    
    * unused dep
    
    * Update ts-packages.yml
    
    * rm-nodes
    
    * move teleporter tests
    
    test-debug
    
    refactor-fix
    
    move teleporter tests over to shared
    
    only fix
    
    * alt<>eth erc1155 test should be the same now
    
    * depcheckrc
    
    * rm-db-tests
    
    * Update ts-packages.yml
    
    * integ test
    
    * consistency check 2
    
    * cleanup
    
    ---------
    
    Co-authored-by: Ino Murko <ino.murko.github@protonmail.com>
    (cherry picked from commit ccad53167a4eac06c18e6442b89e43f79d2f13f2)

---
+++ packages/boba/teleportation/src/data-source.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { HistoryData } from './entity/HistoryData'
+import * as postgres from 'pg' // keep depcheck (db driver)
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.TELEPORTATION_POSTGRES_DB_HOST ?? 'teleportation_db',
+  port: parseInt(process.env.TELEPORTATION_POSTGRES_PORT ?? '5432', 10),
+  username: process.env.TELEPORTATION_POSTGRES_USER ?? 'postgres',
+  password: process.env.TELEPORTATION_POSTGRES_PASSWORD ?? 'abcdef',
+  database: process.env.TELEPORTATION_POSTGRES_DB ?? 'postgres',
+  synchronize: false,
+  logging: false,
+  entities: [HistoryData],
+  migrations: [],
+  subscribers: [],
+})
+
+export const historyDataRepository = AppDataSource.getRepository(HistoryData)
