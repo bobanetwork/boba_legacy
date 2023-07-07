47226d505bb4c29ebf3cd0065caaa19e6af577d5
Riedl Kevin, Bsc <kevin.riedl@wavect.io>
2023-07-07 14:44:22 +0200

    Move DisburserKey to AWS KMS (Teleporter) (#1010)
    * kms sdk v3, ethers, signing works
    
    * approve-works
    
    * integrated-kms-in-service
    
    * integration - final
    
    * env-works
    
    * tests-work
    
    * cleanup
    
    * rm-only
    
    * Delete package-lock.json
    
    * logs
    
    * lint
    
    * fix-avax-seed
    
    * fix-bnb-seed

--- integration-tests/test/alt-l2/teleportation.spec.ts
+++ integration-tests/test/alt-l2/teleportation.spec.ts
@@ -146,7 +146,6 @@ describe('teleportation', () => {
       teleportationAddress: useBnb
         ? TeleportationBNB.address
         : Teleportation.address,
-      disburserWallet: wallet1,
       selectedBobaChains: useBnb ? selectedBobaChainsBnb : selectedBobaChains,
       // only defined one other for the routing tests (so idx 0 = own origin network)
       ownSupportedAssets: useBnb
@@ -154,6 +153,14 @@ describe('teleportation', () => {
         : selectedBobaChainsBnb[0].supportedAssets,
       pollingInterval,
       blockRangePerPolling,
+      awsConfig: {
+        // Default values for local kms endpoint
+        awsKmsAccessKey: process.env.TELEPORTATION_AWS_KMS_ACCESS_KEY ?? '1',
+        awsKmsSecretKey: process.env.TELEPORTATION_AWS_KMS_SECRET_KEY ?? '2',
+        awsKmsKeyId: process.env.TELEPORTATION_AWS_KMS_KEY_ID ?? '3',
+        awsKmsEndpoint: process.env.TELEPORTATION_AWS_KMS_ENDPOINT ?? 'http://kms:8888/',
+        awsKmsRegion: process.env.TELEPORTATION_AWS_KMS_REGION ?? 'us-east-1',
+      },
     })
   }
 
