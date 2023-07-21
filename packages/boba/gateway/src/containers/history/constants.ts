986f5de33bfc08e7e9053eb8cb9d12422acc4b46
Jesus Meza <jemeza@stanford.edu>
2023-07-21 09:17:50 -0500

    adding types to history/constants

--- packages/boba/gateway/src/containers/history/constants.ts
+++ packages/boba/gateway/src/containers/history/constants.ts
@@ -1,18 +1,21 @@
 import AllNetworksIcon from '../../images/allNetworks.svg'
+import { IDropdownItem } from 'components/global/dropdown'
+import { IFilterDropdownItem } from 'components/filter'
+import { TableHeaderOptionType } from 'components/global/table'
 
-export const ALL_NETWORKS = {
+export const ALL_NETWORKS: IDropdownItem = {
   value: 'All',
   label: 'All Networks',
   imgSrc: AllNetworksIcon,
 }
-export const FILTER_OPTIONS = [
+export const FILTER_OPTIONS: IFilterDropdownItem[] = [
   { value: 'All', label: 'All Status' },
   { value: 'Completed', label: 'Completed' },
   { value: 'Pending', label: 'Pending' },
   { value: 'Canceled', label: 'Canceled' },
 ]
 
-export const TableOptions = [
+export const TableOptions: TableHeaderOptionType[] = [
   {
     name: 'Date',
     width: 168,
