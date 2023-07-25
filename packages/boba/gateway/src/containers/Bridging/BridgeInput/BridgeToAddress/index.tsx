c2e2042f4687c63b7e8fec4e1c57a91eea4dcf38
Sahil Kashetwar <sahil@enya.ai>
2023-07-26 03:28:04 +0530

    fix: enable bridge to address for L1 with classical bridge only

--- packages/boba/gateway/src/containers/Bridging/BridgeInput/BridgeToAddress/index.tsx
+++ packages/boba/gateway/src/containers/Bridging/BridgeInput/BridgeToAddress/index.tsx
@@ -1,16 +1,25 @@
-import React, { FC, memo, useState } from 'react'
+import React, { FC, memo, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { selectBridgeToAddressState } from 'selectors'
+import {
+  selectBridgeToAddressState,
+  selectBridgeType,
+  selectLayer,
+} from 'selectors'
 import { ReceiveContainer } from '../styles'
 import { Label } from '../../styles'
 import InputWithButton from 'components/global/inputWithButton'
+import { BRIDGE_TYPE } from 'containers/Bridging/BridgeTypeSelector'
+import { LAYER } from 'util/constant'
 
 type Props = {}
 
 const BridgeToAddress: FC<Props> = ({}) => {
   const bridgeToAddressEnable = useSelector(selectBridgeToAddressState())
+  const layer = useSelector(selectLayer())
+  const bridgeType = useSelector(selectBridgeType())
 
   const [toAddress, setToAddress] = useState('')
+  const [isAvailable, setIsAvailable] = useState(true)
 
   const onAddressChange = (e: any) => {
     const text = e.target.value
@@ -28,7 +37,15 @@ const BridgeToAddress: FC<Props> = ({}) => {
     }
   }
 
-  if (!bridgeToAddressEnable) {
+  useEffect(() => {
+    if (layer !== LAYER.L1 || bridgeType !== BRIDGE_TYPE.CLASSIC) {
+      setIsAvailable(false)
+    } else {
+      setIsAvailable(true)
+    }
+  }, [layer, bridgeType])
+
+  if (!bridgeToAddressEnable || !isAvailable) {
     return null
   }
 
