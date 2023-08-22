1154f7d370d0f1664609f39885cc50a154030a4b
Jesus Meza <jemeza@stanford.edu>
2023-08-22 10:37:04 -0500

    synpress test

--- packages/boba/gateway/cypress.config.ts
+++ packages/boba/gateway/cypress.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'cypress'
+import synpressPlugins from '@synthetixio/synpress/plugins'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+  userAgent: 'synpress',
+  chromeWebSecurity: true,
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 60000,
   e2e: {
-    baseUrl: process.env.BASE_URL,
+    testIsolation: false,
+    setupNodeEvents: (on, config) => {
+      synpressPlugins(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+    supportFile: 'cypress/support/e2e.ts',
   },
 })
