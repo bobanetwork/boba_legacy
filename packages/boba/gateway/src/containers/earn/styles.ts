b49a762b8e43deb9bd7589f780d1c7f2a1d13afd
Sahil Kashetwar <sahil@enya.ai>
2023-09-05 12:26:29 +0530

    fix: style fixes for earn, history and stake page

--- packages/boba/gateway/src/containers/earn/styles.ts
+++ packages/boba/gateway/src/containers/earn/styles.ts
@@ -41,6 +41,11 @@ export const Help = styled.div`
   padding: 10px;
   border-radius: 8px;
   background: ${(props) => props.theme.colors.box.background};
+  ${({ theme: { name, colors } }) => css`
+    border: 1px solid ${name === 'light' ? colors.blue[200] : colors.blue[100]};
+    background: ${name === 'light' ? colors.blue[50] : colors.blue[500]};
+    color: ${name === 'light' ? colors.blue[500] : colors.blue[100]};
+  `}
 `
 
 export const AlertText = styled(Typography)`
@@ -83,6 +88,7 @@ export const EarnActionContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 10px 0px;
 `
 
 export const EarnListContainer = styled.div`
