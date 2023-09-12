986c3995b567a67096cb916fd755ed3ce8602972
Jesus Meza <jemeza@stanford.edu>
2023-09-12 15:39:47 -0500

    increasing timeout

--- packages/boba/gateway/cypress.config.ts
+++ packages/boba/gateway/cypress.config.ts
@@ -6,9 +6,9 @@ dotenv.config()
 export default defineConfig({
   userAgent: 'synpress',
   chromeWebSecurity: true,
-  defaultCommandTimeout: 30000,
-  pageLoadTimeout: 30000,
-  requestTimeout: 30000,
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 60000,
   e2e: {
     testIsolation: false,
     setupNodeEvents: (on, config) => {
