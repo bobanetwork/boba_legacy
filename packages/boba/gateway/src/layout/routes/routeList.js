13ccc0f9c9d1710e33a73850e3c18af398efee62
Sahil Kashetwar <sahil@enya.ai>
2023-09-14 09:43:51 +0530

    chore: cleanup veDao & voteAndDao containers and ref

--- packages/boba/gateway/src/layout/routes/routeList.js
+++ packages/boba/gateway/src/layout/routes/routeList.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-import { DISABLE_VE_DAO, ROUTES_PATH } from "util/constant";
+import { ROUTES_PATH } from "util/constant";
 
 import Bridging from 'containers/Bridging';
-import Vote from 'containers/VoteAndDao/Vote/Vote';
 import BobaScope from 'containers/bobaScope/BobaScope';
 import OldDao from 'containers/dao/OldDao';
 import DevTools from 'containers/devtools/DevTools';
 import EarnWrapper from 'containers/earn/EarnWrapper';
 import Home from "containers/home/Home";
 import SaveWrapper from 'containers/save/SaveWrapper';
-import Lock from 'containers/veboba/Lock';
-import { Navigate } from 'react-router-dom';
+
 import History from 'containers/history/History';
 
 export const COMMON_ROUTES = [
@@ -61,19 +60,7 @@ export const ROUTE_LIST = [
         path: ROUTES_PATH.DAO,
         element: <OldDao />,
         key: 'DAO',
-      },
-      {
-        path: ROUTES_PATH.LOCK,
-        element: <Lock />,
-        key: 'DAO',
-        disable: !!Number(DISABLE_VE_DAO),
-      },
-      {
-        path: ROUTES_PATH.VOTE_DAO,
-        element: <Vote />,
-        key: 'DAO',
-        disable: !!Number(DISABLE_VE_DAO),
-      },
+      }
     ]
   }
 ]
