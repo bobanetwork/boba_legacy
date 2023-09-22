ef44554064f51c3167163b57982f3cdb45d71024
Riedl Kevin, Bsc <kevin.riedl@wavect.io>
2023-09-22 18:40:02 +0200

    feat: Airdrop gas via Light Bridge (#1180)
    * add weth on bsc
    
    * use existing weth
    
    * update-l2-weth
    
    * gateway-correct-messaging
    
    * fix error messages for bridge
    
    * undo-old-code
    
    * destchainid
    
    * teleport
    
    * teleportAsset works
    
    * fixed-err
    
    * cleanup
    
    * monitor-watcher
    
    * monitor-fix
    
    * watcher
    
    * type
    
    * redeploy teleportation after audit on testnets
    
    * Update 020-Teleportation.deploy.ts
    
    * Update 020-Teleportation.deploy.ts
    
    * teleportation service fixed
    
    * error
    
    * fix teleportation service
    
    * Add contracts to register
    
    * EIP1155 support
    
    * fix-build
    
    * teleportAsset fixed
    
    * filters work
    
    * rpc-fixed
    
    * disburse
    
    * checkpoint
    
    * fe part done
    
    * subgraph-teleportation
    
    * graphql
    
    * del
    
    * teleportation integrated in Gateway
    
    * rm teleportation from watcher/serverless
    
    * fix approve for teleportation
    
    * only-show-on-testnet
    
    * fix-merge
    
    * depcheck
    
    * fix-tests
    
    * fix depcheck & linter
    
    * feat: Add info alert for teleportation bridge that unconventional wallets are not supported
    
    * rm errors
    
    * merge
    
    * airdrop progress
    
    * fix linting
    
    * alt
    
    * resolve depcheck
    
    * implement airdrop gas on teleportation feedback
    
    * feedback airdrop implemented: airdrop conditions for teleportation
    
    * rm only
    
    * remove usd/coingecko for teleportation
    
    * integration tests
    
    * rm minusdval
    
    * depcheck fix
    
    ---------
    
    Co-authored-by: cby3149 <cby3149@gmail.com>

--- packages/boba/teleportation/src/utils/kms-signing.ts
+++ packages/boba/teleportation/src/utils/kms-signing.ts
@@ -13,6 +13,8 @@ export interface IKMSSignerConfig {
   awsKmsAccessKey: string
   awsKmsSecretKey: string
   awsKmsKeyId: string
+  /** @dev Should always be enabled, but can be helpful for debugging and unit tests, .. */
+  disableDisburserCheck?: boolean
 }
 
 export class KMSSigner {
